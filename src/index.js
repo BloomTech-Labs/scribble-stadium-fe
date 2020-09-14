b6c25e49950efcd69c77cdbd782b59fd097b9dbd
Schrese <53787633+Schrese@users.noreply.github.com>
2020-09-14 17:23:36 -0400

    Removed commented out code in index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore, applyMiddleware } from 'redux';
-// import { Provider } from 'react-redux';
-// import thunk from 'redux-thunk';
-
-// import { reducer } from './state/reducers';
 
 import {
   BrowserRouter as Router,
@@ -19,23 +14,18 @@ import 'antd/dist/antd.less';
 import { NotFoundPage } from './components/pages/NotFound';
 import { ExampleListPage } from './components/pages/ExampleList';
 import { ProfileListPage } from './components/pages/ProfileList';
-// import { LoginPage } from './components/pages/Login';
 import { HomePage } from './components/pages/Home';
 import { ExampleDataViz } from './components/pages/ExampleDataViz';
 import { config } from './utils/oktaConfig';
 import { LoadingComponent } from './components/common';
 import { LandingPage } from './components/pages/LandingPage';
 
-// const store = createStore(reducer, applyMiddleware(thunk));
-
 ReactDOM.render(
-  // <Provider store={store}>
   <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Router>,
-  // </Provider>,
   document.getElementById('root')
 );
 
