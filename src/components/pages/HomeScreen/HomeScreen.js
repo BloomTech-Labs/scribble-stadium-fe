f7bc7e9e6174c12e45fcc81f1d6f64ab0a2c976a
Sandra Coburn <sandra_coburn@hotmail.com>
2020-09-14 10:36:11 -0700

    Deleted unused code

--- src/components/pages/HomeScreen/HomeScreen.js
+++ src/components/pages/HomeScreen/HomeScreen.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Layout, Menu, Button, Typography } from 'antd';
 
+import { PlusCircleFilled } from '@ant-design/icons';
+
 import '../../../styles/HomeScreen.less';
-import SubscribeButton from '../../common/SubscribeButton';
 
 const { Content, Sider } = Layout;
 const { Title } = Typography;
@@ -36,7 +37,10 @@ class HomeScreen extends Component {
             </Title>
             <Content className="content">
               <button>
-                <h2>Subscribe</h2>
+                <h2>
+                  <PlusCircleFilled />
+                  Add a Child
+                </h2>
               </button>
             </Content>
           </Layout>
@@ -45,47 +49,5 @@ class HomeScreen extends Component {
     );
   }
 }
-
-//   <Layout>
-//     <Sider
-//       className="container"
-//       trigger={null}
-//       collapsible
-//       collapsed={this.state.collapsed}
-//     >
-//       <div className="logo">Welcome Back</div>
-//       <Menu mode="inline" defaultSelectedKeys={['1']}>
-//         <Menu.Item key="1">Dashboard</Menu.Item>
-//         <Menu.Item key="2">Parent Settings</Menu.Item>
-//         <Menu.Item key="3">Help</Menu.Item>
-//         <Menu.Item key="4">Log out</Menu.Item>
-//       </Menu>
-//     </Sider>
-//     <Layout className="site-layout">
-//       <Header className="site-layout-background" style={{ padding: 0 }}>
-//         {React.createElement(
-//           this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-//           {
-//             className: 'trigger',
-//             onClick: this.toggle,
-//           }
-//         )}
-//         <Title level={2}>STORY SQUAD</Title>
-//       </Header>
-//       <Content
-//         className="site-layout-background2"
-//         style={{
-//           margin: '24px 16px',
-//           padding: 24,
-//           minHeight: 280,
-//         }}
-//       >
-//         <SubscribeButton value="large" />
-//       </Content>
-//     </Layout>
-//   </Layout>
-// );
-//   }
-// }
 
 export default HomeScreen;
