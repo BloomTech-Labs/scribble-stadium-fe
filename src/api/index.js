f82ec59f29aa9f15843cd8ebf11f443ea44cb6d8
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-17 13:31:47 -0700

    Added change to make api calls modular and reusable

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // we will define a bunch of API calls here.
-const apiUrl = `${process.env.REACT_APP_API_URI}/profiles`;
+const apiUrl = process.env.REACT_APP_API_URI;
 
 const sleep = time =>
   new Promise(resolve => {
@@ -34,13 +34,15 @@ const getDSData = (url, authState) => {
     .catch(err => err);
 };
 
-const apiAuthGet = authHeader => {
-  return axios.get(apiUrl, { headers: authHeader });
+const apiAuthGet = (endpoint, authHeader) => {
+  return axios.get(`${apiUrl}${endpoint}`, { headers: authHeader });
 };
 
 const getProfileData = authState => {
   try {
-    return apiAuthGet(getAuthHeader(authState)).then(response => response.data);
+    return apiAuthGet('/profiles', getAuthHeader(authState)).then(
+      response => response.data
+    );
   } catch (error) {
     return new Promise(() => {
       console.log(error);
