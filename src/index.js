b8fc412e5eb0d1c1e865a60eb74545c3331e169f
Schrese <53787633+Schrese@users.noreply.github.com>
2020-09-21 18:19:58 -0400

    connected child dashboard to mission control

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 import {
   BrowserRouter as Router,
   Route,
@@ -15,11 +14,13 @@ import 'antd/dist/antd.less';
 import { NotFoundPage } from './components/pages/NotFound';
 import { ExampleListPage } from './components/pages/ExampleList';
 import { ProfileListPage } from './components/pages/ProfileList';
-import { LoginPage } from './components/pages/Login';
+import { LandingPage } from './components/pages/LandingPage';
 import { HomePage } from './components/pages/Home';
 import { ExampleDataViz } from './components/pages/ExampleDataViz';
 import { config } from './utils/oktaConfig';
 import { LoadingComponent } from './components/common';
+import { ChildDashboard } from './components/pages/ChildDashboard';
+import { MissionControl } from './components/pages/MissionControl';
 
 ReactDOM.render(
   //
@@ -44,16 +45,28 @@ function App() {
   };
 
   return (
-   
     <Security {...config} onAuthRequired={authHandler}>
       <Switch>
-        <Route path="/login" component={LoginPage} />
+        <Route path="/login" component={LandingPage} />
         <Route path="/implicit/callback" component={LoginCallback} />
         {/* any of the routes you need secured should be registered as SecureRoutes */}
         <SecureRoute
           path="/"
           exact
           component={() => <HomePage LoadingComponent={LoadingComponent} />}
+        />
+        <SecureRoute
+          path="/child-dashboard"
+          exact
+          component={() => (
+            <ChildDashboard LoadingComponent={LoadingComponent} />
+          )}
+        />
+        <SecureRoute
+          path="/child-dashboard/mission-control"
+          component={() => (
+            <MissionControl LoadingComponent={LoadingComponent} />
+          )}
         />
         <SecureRoute path="/example-list" component={ExampleListPage} />
         <SecureRoute path="/profile-list" component={ProfileListPage} />
