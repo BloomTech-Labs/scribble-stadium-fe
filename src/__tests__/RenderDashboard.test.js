b9fc900571f62f9b94d6784a3caf83885f320975
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-21 16:18:23 -0700

    Update RenderDashboard.test.js

--- src/__tests__/RenderDashboard.test.js
+++ src/__tests__/RenderDashboard.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-import Help from '../components/pages/Help/Help';
+import Help from '../../components/pages/Help/Help';
 
 test('Heading', () => {
   render(
