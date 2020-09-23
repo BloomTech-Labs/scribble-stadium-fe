081c8f6f7cbd175551c9fbea20761e2be1543f16
Schrese <53787633+Schrese@users.noreply.github.com>
2020-09-23 14:26:39 -0400

    removed report

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -9,20 +9,20 @@ const LandingPage = () => {
       <Header />
       <Row id="head-row">
         <Col span={16}>
-          <p>
+          <h6>
             Story Squad is a game where imagination comes to play. It’s where
             generating ideas scores big.
-          </p>
-          <p>
+          </h6>
+          <h6>
             Story Squad springs storytellers into action by partnering them up
             to participate in interactive & immersive creative challenges.
-          </p>
-          <p>
+          </h6>
+          <h6>
             Become a master of your craft by submitting original drawings &
             handwritten stories, receiving and giving real feedback, sharing
             points in a squad-vs-squad matchup, and finally see if you won.
-          </p>
-          <p>Ready?</p>
+          </h6>
+          <h6>Ready?</h6>
         </Col>
         <Col span={8}>
           <LoginContainer />
