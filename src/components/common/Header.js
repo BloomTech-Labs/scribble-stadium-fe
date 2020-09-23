688162d7bd0defded362fa0bc7a14771faf95b28
Schrese <53787633+Schrese@users.noreply.github.com>
2020-09-23 18:57:59 -0400

    updated header to be more semantic small tweaks to landing page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import city_img from '../../assets/images/cityscape.svg';
 
 const Header = () => {
   return (
     <div className="hero">
-      <p className="header-text">STORY SQUAD</p>
-      {/* <img className="city-image" src={city_img} alt="MyCity" /> */}
+      <h1 className="header-text">STORY SQUAD</h1>
     </div>
   );
 };
