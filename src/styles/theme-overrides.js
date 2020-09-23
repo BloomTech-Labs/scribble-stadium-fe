11c86988f82161bdd58b0421260baa3edbf6148f
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-23 15:22:13 -0700

    Restyled profile modal to match the spec and use fun little antd thingies.

--- src/styles/theme-overrides.js
+++ src/styles/theme-overrides.js
@@ -1,23 +1,30 @@
 module.exports = {
-  '@primary-color': '#6ceae6', // Turquoise
-  '@link-color': ' #429cb5', // Boston Blue
-  '@success-color': '#00d93d', // Border around story submitted modal
-  '@warning-color': '#ff313a', // from 'remove child' button
-  '@error-color': '#f5222d', // Un-changed
-  '@font-size-base': '18px', // Base font size
-  '@heading-color': '#292929', // Header-Nero
-  '@text-color': '#5F5F5F', // Body Text-Dim Grey
-  '@text-color-secondary': '#949494', // Secondary Text-Suva Grey
-  '@disabled-color': 'rgba(0, 0, 0, 0.25)',
+  // '@primary-color': '#6ceae6', // Turquoise
+  '@primary-color': '#007aff', // Turquoise
+  // '@link-color': ' #429cb5', // Boston Blue
+  // '@success-color': '#00d93d', // Border around story submitted modal
+  // '@warning-color': '#ff313a', // from 'remove child' button
+  // '@error-color': '#f5222d', // Un-changed
+  // '@font-size-base': '18px', // Base font size
+  // '@heading-color': '#292929', // Header-Nero
+  // '@text-color': '#5F5F5F', // Body Text-Dim Grey
+  // '@text-color-secondary': '#949494', // Secondary Text-Suva Grey
+  // '@disabled-color': 'rgba(0, 0, 0, 0.25)',
   '@border-radius-base': '10px', // Most used border-radius
-  '@border-color-base': '#292929', // Header-Nero
-  '@box-shadow-base':
-    '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)',
-  '@body-background': '##444444', // main 'background' for desktop
-  '@btn-primary-color': '#007aff', // parent btn color
-  '@btn-default-color': '#eb7d5b', // Child btn color (Burnt Sienna)
-  '@btn-danger-color': '#ff313a', // From parent dashboard
-  '@checkbox-size': '50px', // From mission dash
-  '@checkbox-color': '#429cb5', // Block Background-White
-  '@checkbox-check-color': '#ffffff', // Block Background-White
+  // '@border-color-base': '#292929', // Header-Nero
+  // '@box-shadow-base':
+  //   '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)',
+  // '@body-background': '##444444', // main 'background' for desktop
+  // '@btn-primary-color': '#007aff', // parent btn color
+  // '@btn-default-color': '#eb7d5b', // Child btn color (Burnt Sienna)
+  // '@btn-danger-color': '#ff313a', // From parent dashboard
+  // '@checkbox-size': '50px', // From mission dash
+  // '@checkbox-color': '#429cb5', // Block Background-White
+  // '@checkbox-check-color': '#ffffff', // Block Background-White
+  // Card Styles
+  '@card-radius': '14px',
+
+  // Buttons
+  '@btn-primary-color': '#fff',
+  '@btn-primary-bg': '@primary-color',
 };
