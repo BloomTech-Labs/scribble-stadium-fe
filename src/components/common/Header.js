a688816072c87957fa1eb6c4f0343a6fc22c5319
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-24 09:45:25 -0700

    Implemented menu button with basic styling .

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
-const Header = () => {
+import { Link } from 'react-router-dom';
+import { Button, Dropdown, Menu } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+
+const menu = (
+  <Menu>
+    <Menu.Item key="1">
+      <Link to="/child/dashboard">Home</Link>
+    </Menu.Item>
+    <Menu.Item key="2">
+      <Link>Help</Link>
+    </Menu.Item>
+    <Menu.Item key="3">
+      <Link>Change User</Link>
+    </Menu.Item>
+    <Menu.Item key="4">
+      <Link>Log Out</Link>
+    </Menu.Item>
+  </Menu>
+);
+
+const Header = props => {
   return (
     <div className="hero">
+      <Dropdown overlay={menu} trigger={['click']} className="menu-button">
+        <Button icon={<MenuOutlined />} type="text" />
+      </Dropdown>
       <h1 className="header-text">STORY SQUAD</h1>
     </div>
   );
