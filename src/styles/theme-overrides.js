ed6c1e383bdc911bb75693f20ec067cbdd05e4ba
Sandra Coburn <sandra_coburn@hotmail.com>
2020-09-25 07:21:43 -0700

    Added variable for background color to theme overrides

--- src/styles/theme-overrides.js
+++ src/styles/theme-overrides.js
@@ -24,6 +24,7 @@ module.exports = {
   // Card Styles
   '@card-radius': '14px',
 
+  '@layout-body-background': '#fff',
   // Buttons
   '@btn-primary-color': '#fff',
   '@btn-primary-bg': '@primary-color',
