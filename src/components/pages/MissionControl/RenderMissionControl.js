745c3b818a620d2f89ef247ce77c24e9b67d6998
Schrese <53787633+Schrese@users.noreply.github.com>
2020-09-28 17:27:30 -0400

    updates to home and mission-control routing links

--- src/components/pages/MissionControl/RenderMissionControl.js
+++ src/components/pages/MissionControl/RenderMissionControl.js
@@ -2,22 +2,30 @@ import React from 'react';
 import { Header } from '../../common';
 import { Row, Col, Checkbox } from 'antd';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import draw_icon from '../../../assets/icons/draw_icon.svg';
 import read_icon from '../../../assets/icons/read_icon.svg';
 import write_icon from '../../../assets/icons/write_icon.svg';
 
 const RenderMissionControl = props => {
+  const { push } = useHistory();
+
   function handleChecked(e) {
     console.log(`checked=${e.target.checked}`);
   }
+
+  const handleReadStory = e => {
+    console.log('get to reading!!!');
+    push('/child/story');
+  };
 
   return (
     <>
       <Header />
       <div className="mission-container">
         <Row className="main-row">
-          <Col className="read" span={12}>
+          <Col className="read" span={12} onClick={handleReadStory}>
             <Checkbox
               className="checking-box"
               defaultChecked={false}
@@ -25,9 +33,7 @@ const RenderMissionControl = props => {
             />
             <Col className="image-and-text-container">
               <img src={read_icon} alt="reading icon" />
-              <p className="mission-control-text">
-                <Link to="/child/story">Read</Link>
-              </p>
+              <p className="mission-control-text">Read</p>
             </Col>
           </Col>
           <Col className="write-and-draw" span={12}>
