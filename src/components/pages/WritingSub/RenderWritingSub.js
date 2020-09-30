5aff93211ae4ba841e55bd0f3cb62ebcc4f5607a
Sandra Coburn <sandra_coburn@hotmail.com>
2020-09-30 15:05:40 -0700

    Added submission modal to writingsub component

--- src/components/pages/WritingSub/RenderWritingSub.js
+++ src/components/pages/WritingSub/RenderWritingSub.js
@@ -3,12 +3,17 @@ import { Header } from '../../common';
 import { Row } from 'antd';
 import { UploadDocs } from '../../common/';
 import { postNewWritingSub } from '../../../api/index';
+import { SubmissionModal } from '../../common/index';
 
 const RenderWritingSub = () => {
+  const inst =
+    'Once you finish writing your story, please take a picture of all your pages and upload them. After all pages are uploaded, click submit.';
   const submissionId = 1;
+
   return (
     <>
       <Header title="PENCILS READY?" />
+      <SubmissionModal instructions={inst} />
       <div className="writing-sub-container">
         <Row className="main-row">
           <p>
