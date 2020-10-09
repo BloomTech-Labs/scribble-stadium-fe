e0272f9b6eb2158aebf5cbdd13b77a767b45931b
Sandra Coburn <sandra_coburn@hotmail.com>
2020-10-09 15:58:42 -0700

    Tests for ProfileModalContainer

---
+++ src/__tests__/ProfileModalContainer.test.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { configure, shallow, ShallowWrapper } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
+import LoadingComponent from '../components/common/ParentLoadingComponent';
+import ProfileRenderModal from '../components/pages/Modal/ProfileRenderModal';
+import ProfileModalContainer from '../components/pages/Modal/ProfileModalContainer';
+jest.mock('@okta/okta-react', () => ({
+  useOktaAuth: () => {
+    return {
+      authState: {
+        isAuthenticated: true,
+      },
+      authService: {},
+    };
+  },
+}));
+
+configure({ adapter: new Adapter() });
+
+describe('<ProfileModalContainer />', () => {
+  configure({ adapter: new Adapter() });
+  const mockStore = configureStore([]);
+  const store = mockStore();
+
+  describe('Render <ProfileModalContainer />', () => {
+    let shallowWrapper = ShallowWrapper;
+    beforeEach(() => {
+      shallowWrapper = shallow(
+        <Router>
+          <Provider store={store}>
+            <ProfileModalContainer />
+          </Provider>
+        </Router>
+      );
+    });
+
+    it('Find ProfileRenderModal', () => {
+      expect(shallowWrapper.find(ProfileRenderModal).length).toBe(0);
+    });
+    it('Find Loading Component', () => {
+      expect(shallowWrapper.find(LoadingComponent));
+    });
+  });
+});
