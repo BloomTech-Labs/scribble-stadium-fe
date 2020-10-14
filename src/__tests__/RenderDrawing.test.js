77e728975ddd44296c7f1521a21b815063dc513a
Schrese <53787633+Schrese@users.noreply.github.com>
2020-10-14 16:52:07 -0400

    updated tests to accomodate state changes

--- src/__tests__/RenderDrawing.test.js
+++ src/__tests__/RenderDrawing.test.js
@@ -19,18 +19,20 @@ describe('<RenderDrawingSub />', () => {
 
   it('Should render <SubmissionModal />', () => {
     wrapper = shallow(
-      <RenderDrawingSub instructions={() => {}} tasks={() => {}} />
+      <RenderDrawingSub instructions={() => {}} tasks={{ story: {} }} />
     );
     wrapper.setProps({ instructions: { inst: '' } });
     expect(wrapper.find(SubmissionModal)).toHaveLength(1);
   });
   it('Should render <Header />', () => {
-    wrapper = shallow(<RenderDrawingSub title={() => {}} tasks={() => {}} />);
+    wrapper = shallow(
+      <RenderDrawingSub title={() => {}} tasks={{ story: {} }} />
+    );
     wrapper.setProps({ title: { title: '' } });
     expect(wrapper.find(Header)).toHaveLength(1);
   });
   it('should render a Button in the antD form', () => {
-    wrapper = shallow(<RenderDrawingSub tasks={() => {}} />);
+    wrapper = shallow(<RenderDrawingSub tasks={{ story: {} }} />);
     expect(wrapper.find(Button));
   });
 });
