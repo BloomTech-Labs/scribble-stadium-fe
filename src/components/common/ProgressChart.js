b572989789ac44ce6f785b5804c3f68f8ea74139
Sandra Coburn <sandra_coburn@hotmail.com>
2020-10-19 14:54:14 -0700

    Created a file to hold testing data

---
+++ src/components/common/ProgressChart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+import { data } from '../../utils/dataTest';
+
+const ProgressChart = () => {
+  return (
+    <div>
+      <h2> Data visualization</h2>
+      <div>
+        <Plot
+          data={data.data}
+          layout={{ width: 420, height: 340, title: 'Weekly Progress' }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProgressChart;
