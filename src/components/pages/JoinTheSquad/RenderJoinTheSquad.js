1915b65dd408edda2a136407497fbe0337df01c9
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-10-20 18:09:53 -0500

    styling done, need to add props for name and add useeffect to get squad

--- src/components/pages/JoinTheSquad/RenderJoinTheSquad.js
+++ src/components/pages/JoinTheSquad/RenderJoinTheSquad.js
@@ -4,6 +4,8 @@ import { Col, Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react/dist/OktaContext';
 import Squadup from '../../../assets/images/Squadup.svg';
+import wordbubble from '../../../assets/images/match_up_images/wordbubble.svg';
+import wordBubbleright from '../../../assets/images/match_up_images/wordBubbleright.svg';
 
 
 
@@ -19,15 +21,20 @@ const RenderJoinTheSquad = props => {
 
         <Col className="joinSquad1" xs={24} sm={12}>
           <div className="imgContain1">
-            <img src={Squadup} alt="Blast Character Background" />
+            <p className="text">Hi! <br></br>My name is WhiteFox.</p>
+            <img className="wordBubble" src={wordbubble} alt="word bubble" />
+            <img className="star" src={Squadup} alt="Blast Character Background" />
           </div>
+
         </Col>
-        <Col className="joinSquad2"xs={24} sm ={12}>
+        <Col className="joinSquad2" xs={24} sm={12}>
           <div className="imgContain2">
-            <img className='star'src={Squadup} alt="Blast Character Background" />
+          <p className="text2">Hi! <br></br>My name is WhiteFox.</p>
+            <img className="wordBubble2" src={wordBubbleright} alt="word bubble" />
+            <img className='star' src={Squadup} alt="Blast Character Background" />
           </div>
           <div className="button">
-          <Button selection="#eb7d5bbb" className="sharePoints" type="primary" size="large">Share Points</Button>
+            <Button selection="#eb7d5bbb" className="sharePoints" type="primary" size="large">Share Points</Button>
           </div>
         </Col>
       </div>
