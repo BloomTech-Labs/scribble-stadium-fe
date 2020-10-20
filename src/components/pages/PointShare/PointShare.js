58caefde284d78271463741ae2ec353dddc2371c
James <jlc6290@gmail.com>
2020-10-20 12:09:18 -0600

    initial state logic added

--- src/components/pages/PointShare/PointShare.js
+++ src/components/pages/PointShare/PointShare.js
@@ -1,73 +1,33 @@
 import React, { useState } from 'react';
 import { Header } from '../../common';
-import { Row, Col } from 'antd';
+import { Row, Col, InputNumber, Button } from 'antd';
+import { connect } from 'react-redux';
 
 import placeholder from '../../../assets/images/child_dashboard_images/change_your_avatar.svg';
-import { CaretUpFilled, CaretDownFilled } from '@ant-design/icons';
 
 const PointShare = props => {
-  const [totalPoints_A, setTotalPoints_A] = useState(100);
-  const [totalPoints_B, setTotalPoints_B] = useState(100);
+  const [totalPoints, setTotalPoints] = useState(100);
   const [storyOnePoints, setStoryOnePoints] = useState(0);
   const [storyTwoPoints, setStoryTwoPoints] = useState(0);
   const [illustrationOnePoints, setIllustrationOnePoints] = useState(0);
   const [illustrationTwoPoints, setIllustrationTwoPoints] = useState(0);
 
-  const incrementStoryPoints_A = () => {
-    if (totalPoints_A > 0) {
-      setStoryOnePoints(storyOnePoints + 1);
-      setTotalPoints_A(totalPoints_A - 1);
-    }
+  const checkValues = () => {
+    setTotalPoints(prevState => {
+      var submittedTotal =
+        storyOnePoints +
+        storyTwoPoints +
+        illustrationOnePoints +
+        illustrationTwoPoints;
+      if (submittedTotal > 100) {
+        return prevState;
+      } else {
+        // update header prop
+      }
+    });
   };
 
-  const decrementStoryPoints_A = () => {
-    if (totalPoints_A > 0) {
-      setStoryOnePoints(storyOnePoints - 1);
-      setTotalPoints_A(totalPoints_A + 1);
-    }
-  };
-
-  const incrementStoryPoints_B = () => {
-    if (totalPoints_B > 0) {
-      setStoryTwoPoints(storyTwoPoints + 1);
-      setTotalPoints_B(totalPoints_B - 1);
-    }
-  };
-
-  const decrementStoryPoints_B = () => {
-    if (totalPoints_B > 0) {
-      setStoryTwoPoints(storyTwoPoints - 1);
-      setTotalPoints_B(totalPoints_B + 1);
-    }
-  };
-
-  const incrementIllustrationPoints_A = () => {
-    if (totalPoints_A > 0) {
-      setIllustrationOnePoints(illustrationOnePoints + 1);
-      setTotalPoints_A(totalPoints_A - 1);
-    }
-  };
-
-  const decrementIllustrationPoints_A = () => {
-    if (totalPoints_A > 0) {
-      setIllustrationOnePoints(illustrationOnePoints - 1);
-      setTotalPoints_A(totalPoints_A + 1);
-    }
-  };
-
-  const incrementIllustrationPoints_B = () => {
-    if (totalPoints_B > 0) {
-      setIllustrationTwoPoints(illustrationTwoPoints + 1);
-      setTotalPoints_B(totalPoints_B - 1);
-    }
-  };
-
-  const decrementIllustrationPoints_B = () => {
-    if (totalPoints_B > 0) {
-      setIllustrationOnePoints(illustrationTwoPoints - 1);
-      setTotalPoints_B(totalPoints_B + 1);
-    }
-  };
+  const formSubmit = () => {};
 
   return (
     <>
@@ -93,29 +53,45 @@ const PointShare = props => {
           <Row className="teammate-one-points">
             <div className="submission-container">
               <img className="submission" src="" alt="Submission" />
-              <h3 className="points">{storyOnePoints}</h3>
-              <CaretUpFilled onClick={incrementStoryPoints_A} />
-              <CaretDownFilled onClick={decrementStoryPoints_A} />
+              <InputNumber
+                defaultValue={storyOnePoints}
+                min={10}
+                step={5}
+                onChange={checkValues}
+                onClick={event => setStoryOnePoints(event.target.value)}
+              />
             </div>
             <div className="submission-container">
               <img className="submission" src="" alt="Submission" />
-              <h3 className="points">{illustrationOnePoints}</h3>
-              <CaretUpFilled onClick={incrementIllustrationPoints_A} />
-              <CaretDownFilled onClick={decrementIllustrationPoints_A} />
+              <InputNumber
+                defaultValue={illustrationOnePoints}
+                min={10}
+                step={5}
+                onChange={checkValues}
+                onClick={event => setIllustrationOnePoints(event.target.value)}
+              />
             </div>
           </Row>
           <Row className="teammate-two-points">
             <div className="submission-container">
               <img className="submission" src="" alt="Submission" />
-              <h3 className="points">{storyTwoPoints}</h3>
-              <CaretUpFilled onClick={incrementStoryPoints_B} />
-              <CaretDownFilled onClick={decrementStoryPoints_B} />
+              <InputNumber
+                defaultValue={storyTwoPoints}
+                min={10}
+                step={5}
+                onChange={checkValues}
+                onClick={event => setStoryTwoPoints(event.target.value)}
+              />
             </div>
             <div className="submission-container">
               <img className="submission" src="" alt="Submission" />
-              <h3 className="points">{illustrationTwoPoints}</h3>
-              <CaretUpFilled onClick={incrementIllustrationPoints_B} />
-              <CaretDownFilled onClick={decrementIllustrationPoints_B} />
+              <InputNumber
+                defaultValue={illustrationTwoPoints}
+                min={10}
+                step={5}
+                onChange={checkValues}
+                onClick={event => setIllustrationTwoPoints(event.target.value)}
+              />
             </div>
           </Row>
         </Col>
@@ -124,4 +100,9 @@ const PointShare = props => {
   );
 };
 
-export default PointShare;
+export default connect(
+  state => ({
+    child: state.child,
+  }),
+  {}
+)(PointShare);
