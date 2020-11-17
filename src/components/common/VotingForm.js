bea59ec4086cc27108ca11502d503bc195798e71
Nathan Cowley <nathancowley12@gmail.com>
2020-11-16 22:57:22 -0700

    forgot to add these files to last commit

--- src/components/common/VotingForm.js
+++ src/components/common/VotingForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Picker from 'emoji-picker-react';
 import { Button, Form, Radio } from 'antd';
 import { useOktaAuth } from '@okta/okta-react';
 import { useHistory } from 'react-router-dom';
@@ -9,15 +10,25 @@ const VotingForm = props => {
   const { push } = useHistory();
   const { authState } = useOktaAuth();
   const [value, setValue] = useState(1);
+  const [chosenEmoji1, setChosenEmoji1] = useState(null);
+  const [chosenEmoji2, setChosenEmoji2] = useState(null);
+
+  const onEmojiClick1 = (event, emojiObject) => {
+    setChosenEmoji1(emojiObject);
+  };
+
+  // const onEmojiClick2 = (e, emojiObj) = {
+  //   setChosenEmoji2(emojiObj)
+  // }
 
   const onChange = e => {
     setValue(e.target.value);
   };
   const onFinish = () => {
     const body = {
-      "Vote": value, 
-      "MemberID": props.MemberID,
-      "FaceoffID": props.FaceoffID,
+      Vote: value,
+      MemberID: props.MemberID,
+      FaceoffID: props.FaceoffID,
     };
     postVotes(authState, body).then(res => {
       push('/child/match-up');
@@ -37,6 +48,14 @@ const VotingForm = props => {
       <Form.Item name="vote" valuePropName="checked">
         <Radio.Group onChange={onChange} value={value}>
           <Radio className="left-radio" value={1} />
+          {/* <div className="emoji-picker">
+            {chosenEmoji1 ? (
+              <span>You chose: {chosenEmoji1.emoji}</span>
+            ) : (
+              <span>No emoji Chosen</span>
+            )}
+            <Picker onEmojiClick1={onEmojiClick1} />
+          </div> */}
 
           <Radio className="right-radio" value={2} />
         </Radio.Group>
--- src/components/pages/VotingPage/RenderVotingPage.js
+++ src/components/pages/VotingPage/RenderVotingPage.js
@@ -6,33 +6,36 @@ import { VotingForm } from '../../common';
 const RenderVotingPage = props => {
   return (
     <>
-      <Header title="VOTE FOR YOUR FAVORITE STORY" />
+      <Header title="VOTE FOR YOUR FAVORITE STORY" displayMenu={true} />
 
       <div className="voting-container">
         <Row className="main-row">
           <Col className="left-half" xs={24} sm={12}>
             <div className="image-and-check-container">
-              <Card className="pdfCard">
-                <img
-                  className="WritingandDrawingIcon"
-                  src={props.faceoff.Submission1.ImgURL}
-                  alt="writing submission"
-                />
-              </Card>
+              {/* <Card className="pdfCard"> */}
+              <img
+                className="WritingandDrawingIcon"
+                src={props.faceoff.Submission1.ImgURL}
+                alt="writing submission"
+              />
+              {/* </Card> */}
             </div>
           </Col>
 
           <Col className="right-half" xs={24} sm={12}>
             <div className="image-and-check-container">
-              <Card className="pdfCard">
-                <img
-                  className="WritingandDrawingIcon"
-                  src={props.faceoff.Submission2.ImgURL}
-                  alt="writing submission"
-                />
-              </Card>
+              {/* <Card className="pdfCard"> */}
+              <img
+                className="WritingandDrawingIcon"
+                src={props.faceoff.Submission2.ImgURL}
+                alt="writing submission"
+              />
+              {/* </Card> */}
             </div>
-            <VotingForm FaceoffID={props.faceoff.ID} MemberID={props.child.memberId}/>
+            <VotingForm
+              FaceoffID={props.faceoff.ID}
+              MemberID={props.child.memberId}
+            />
           </Col>
         </Row>
       </div>
