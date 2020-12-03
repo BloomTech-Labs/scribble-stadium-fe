69f0340531ff0a612f4992e7724bc2262753d91c
DeeDowns <dee_downs@outlook.com>
2020-12-02 19:49:29 -0500

    moved emoji component to repo

---
+++ src/components/common/EmojiPicker.js
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from 'react';
+import { Card } from 'antd';
+//See https://unicode.org/emoji/charts/full-emoji-list.html for unicode equivalents
+const emojiList = [
+  '😀',
+  '😃',
+  '😄',
+  '😁',
+  '😆',
+  '😅',
+  '🤣',
+  '😂',
+  '🙂',
+  '🙃',
+  '😉',
+  '😇',
+  '🤩',
+  '😋',
+  '😛',
+  '😜',
+  '🤪',
+  '😝',
+  '🤑',
+  '🤭',
+  '🤫',
+  '🤔',
+  '🤐',
+  '🤨',
+  '😐',
+  '😑',
+  '😶',
+  '😏',
+  '😒',
+  '🙄',
+  '😬',
+  '🤥',
+  '😌',
+  '😔',
+  '😪',
+  '🤤',
+  '😴',
+  '😷',
+  '🤒',
+  '🤕',
+  '🤢',
+  '🤮',
+  '🤧',
+  '🥵',
+  '🥶',
+  '🥴',
+  '😵',
+  '🤯',
+  '🤠',
+  '🥳',
+  '😎',
+  '🤓',
+  '🧐',
+  '😕',
+  '😟',
+  '🙁',
+  '☹',
+  '😮',
+  '😯',
+  '😲',
+  '😳',
+  '🥺',
+  '😦',
+  '😧',
+  '😨',
+  '😰',
+  '😥',
+  '😢',
+  '😭',
+  '😱',
+  '😖',
+  '😣',
+  '😞',
+  '😓',
+  '😩',
+  '😫',
+  '🥱',
+  '😤',
+];
+//Use this to set the maximum number of emojis a user can submit
+const emojiLimit = 6;
+const Emoji = props => {
+  const { emoji, handleClick } = props;
+  return (
+    <button className="Emoji" onClick={() => handleClick(emoji)}>
+      {emoji}
+    </button>
+  );
+};
+const EmojiPicker = props => {
+  const { getChildState } = props;
+  const [selectedEmojis, setSelectedEmojis] = useState([]);
+  //If parent component wants to get this state, pass cb into props. Converts Array to String of emojis to match backend data type.
+  useEffect(() => {
+    const selectedEmojisString = selectedEmojis.join('') + ',';
+    if (getChildState) {
+      getChildState(selectedEmojisString);
+    }
+  }, [selectedEmojis, getChildState]);
+  const handleAddEmoji = emoji => {
+    if (selectedEmojis.length < emojiLimit && !selectedEmojis.includes(emoji)) {
+      setSelectedEmojis([...selectedEmojis, emoji]);
+    }
+  };
+  const handleRemoveEmoji = emoji => {
+    setSelectedEmojis(
+      selectedEmojis.filter(selectedEmoji => selectedEmoji !== emoji)
+    );
+  };
+  return (
+    <div className="EmojiPicker">
+      <Card title="Give Feedback" className="emoji-feedback-card">
+        <div>
+          {selectedEmojis.map(emoji => (
+            <Emoji emoji={emoji} handleClick={handleRemoveEmoji} />
+          ))}
+        </div>
+      </Card>
+      <Card className="emoji-selection-card">
+        {emojiList.map(emoji => (
+          <Emoji emoji={emoji} handleClick={handleAddEmoji} />
+        ))}
+      </Card>
+    </div>
+  );
+};
+export default EmojiPicker;
