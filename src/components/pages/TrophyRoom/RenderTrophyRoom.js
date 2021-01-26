0bb94abfbd6b7a80a9a3d2f1f0fff43f5f82155c
Steven Sussman <sjsussman@gmail.com>
2021-01-26 17:50:48 -0500

    adding styling for inventory and achievement modals

--- src/components/pages/TrophyRoom/RenderTrophyRoom.js
+++ src/components/pages/TrophyRoom/RenderTrophyRoom.js
@@ -53,15 +53,21 @@ const RenderTrophyRoom = props => {
         visible={isModal1Visible}
         onOk={handle1Ok}
         onCancel={handle1Cancel}
+        width={900}
       >
-        <p>Completed first Mission!</p>
-        <p>Won a game!</p>
-        <p>Got most points for drawing in a mission!</p>
-        <p>Got most points for writing in a mission!</p>
-        <p>Completed 3 missions in a row!</p>
-        <p>Completed 5 missions in a row!</p>
-        <p>Compelted 10 Missions in a row!</p>
-        {/* Map over achievement state here */}
+        <div style={{ overflow: 'scroll' }}>
+          <h3>Completed (Way to go!):</h3>
+          <p>Completed first Mission!</p>
+          <p>Won a game!</p>
+          <p>Got most points for drawing in a mission!</p>
+          <p>Got most points for writing in a mission!</p>
+          <p>Completed 3 missions in a row!</p>
+          <h3>Incomplete (Keep trying!):</h3>
+          <p>Completed 5 missions in a row!</p>
+          <p>Completed 10 Missions in a row!</p>
+          <p>Most Colorful Drawing</p>
+          {/* Map over achievement state here */}
+        </div>
       </Modal>
       <Divider />
       <h2 className="h2" onClick={showInventoryModel}>
@@ -72,10 +78,11 @@ const RenderTrophyRoom = props => {
         visible={isModal2Visible}
         onOk={handle2Ok}
         onCancel={handle2Cancel}
+        style={{ margin: '0 auto' }}
       >
-        <p>Skins</p>
-        <p>Stickers</p>
-        <p>Collectibles</p>
+        <h3>Skins</h3>
+        <h3>Stickers</h3>
+        <h3>Collectibles</h3>
         {/* Map over inventory state here */}
       </Modal>
     </>
