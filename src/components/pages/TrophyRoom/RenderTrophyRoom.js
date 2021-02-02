46d158f33c5850c2343d11bbc6f4e73a3b07174b
Steven Sussman <sjsussman@gmail.com>
2021-02-02 16:19:12 -0500

    combined scoreboard and trophy room styles on comp

--- src/components/pages/TrophyRoom/RenderTrophyRoom.js
+++ src/components/pages/TrophyRoom/RenderTrophyRoom.js
@@ -44,58 +44,61 @@ const RenderTrophyRoom = props => {
   return (
     <>
       <Header displayMenu={true} title="Trophy Room" />
-      <h1>Leader Board</h1>
-      <Leaderboard />
-      <Divider />
-      <h2 className="h2" onClick={showAchievementsModal}>
-        Achievements
-      </h2>
-      <Modal
-        title="Achievements"
-        visible={isAchievementModalVisible}
-        onOk={handleAchievmentOk}
-        onCancel={handleAchievementCancel}
-        width={900}
-      >
-        <div style={{ overflow: 'scroll' }}>
-          <h3>Completed (Way to go!):</h3>
-          {/* {achievementState.map(achievement => {
+      <div className="trophy-container">
+        <Leaderboard child={props.child} />
+        <div className="custom-divider"></div>
+        <div className="modal-container">
+          <h2 className="h2" onClick={showAchievementsModal}>
+            Achievements
+          </h2>
+          <Modal
+            title="Achievements"
+            visible={isAchievementModalVisible}
+            onOk={handleAchievmentOk}
+            onCancel={handleAchievementCancel}
+            width={900}
+          >
+            <div style={{ overflow: 'scroll' }}>
+              <h3>Completed (Way to go!):</h3>
+              {/* {achievementState.map(achievement => {
             if (achievement.completed === true) {
               return <p>{achievement}</p>;
             }
           })} */}
-          <p>Completed first Mission!</p>
-          <p>Won a game!</p>
-          <p>Got most points for drawing in a mission!</p>
-          <p>Got most points for writing in a mission!</p>
-          <p>Completed 3 missions in a row!</p>
-          <h3>Incomplete (Keep trying!):</h3>
-          <p>Completed 5 missions in a row!</p>
-          <p>Completed 10 Missions in a row!</p>
-          <p>Most Colorful Drawing</p>
-          {/* {achievementState.map(achievement => {
+              <p>Completed first Mission!</p>
+              <p>Won a game!</p>
+              <p>Got most points for drawing in a mission!</p>
+              <p>Got most points for writing in a mission!</p>
+              <p>Completed 3 missions in a row!</p>
+              <h3>Incomplete (Keep trying!):</h3>
+              <p>Completed 5 missions in a row!</p>
+              <p>Completed 10 Missions in a row!</p>
+              <p>Most Colorful Drawing</p>
+              {/* {achievementState.map(achievement => {
             if (achievement.completed === false) {
               return <p>{achievement}</p>;
             }
           })} */}
+            </div>
+          </Modal>
+          <Divider />
+          <h2 className="h2" onClick={showInventoryModel}>
+            Inventory
+          </h2>
+          <Modal
+            title="Inventory List"
+            visible={isInventoryModalVisible}
+            onOk={handleInventoryOk}
+            onCancel={handleInventoryCancel}
+            style={{ margin: '0 auto' }}
+          >
+            <h3>Skins</h3>
+            <h3>Stickers</h3>
+            <h3>Collectibles</h3>
+            {/* Map over inventory state here */}
+          </Modal>
         </div>
-      </Modal>
-      <Divider />
-      <h2 className="h2" onClick={showInventoryModel}>
-        Inventory
-      </h2>
-      <Modal
-        title="Inventory List"
-        visible={isInventoryModalVisible}
-        onOk={handleInventoryOk}
-        onCancel={handleInventoryCancel}
-        style={{ margin: '0 auto' }}
-      >
-        <h3>Skins</h3>
-        <h3>Stickers</h3>
-        <h3>Collectibles</h3>
-        {/* Map over inventory state here */}
-      </Modal>
+      </div>
     </>
   );
 };
