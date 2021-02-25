00993b6e8496b39e8fb34a48525bb70e6c26a05a
unknown <tompsherman@gmail.com>
2021-02-25 11:23:53 -0500

    add useEnlargeCenter component

---
+++ src/components/pages/Animations/AnimationComponents/useEnlargeCenter.js
@@ -0,0 +1,24 @@
+import { useSpring } from 'react-spring';
+
+const useEnlargeCenter = ref => {
+  const spring = useSpring({
+    from: {
+      opacity: 0,
+      transform: 'scale(1)',
+    },
+    to: [
+      {
+        opacity: 1,
+        transform: 'scale(4)',
+      },
+      {
+        opacity: 1,
+        transform: 'scale(2)',
+      },
+    ],
+    ref: ref,
+  });
+  return spring;
+};
+
+export default useEnlargeCenter;
