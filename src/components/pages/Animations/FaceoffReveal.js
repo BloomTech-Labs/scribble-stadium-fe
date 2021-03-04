8a4bdf8f4eff2c91d1216c417fdf354f949dfd4a
Brian Kubes <briankubes@gmail.com>
2021-03-03 19:43:30 -0800

    removed some comments on code and adjusted 'GoBackButton' spacing

--- src/components/pages/Animations/FaceoffReveal.js
+++ src/components/pages/Animations/FaceoffReveal.js
@@ -243,42 +243,40 @@ const FaceoffReveal = props => {
         />
       </div>
       {/* intial avatar images to display at top: */}
-
       <animated.img
         className="move-left-move-right"
         style={enlargeMoveLeftStyle}
         src={userAvatar}
         height={topAvatarHeight}
       />
-
       <animated.img
         className="move-left-move-right"
         style={enlargeMoveRightStyle}
         src={opponentAvatar}
         height={topAvatarHeight}
       />
-
-      <animated.h1
+      <animated.h2
         style={goBackButtonStyle}
         onClick={goBacktoMatchup}
         className="go-back-button"
       >
         go back
-      </animated.h1>
+      </animated.h2>
+
       {/* winner's name and points won: */}
       <div className="bottom-fixed">
         <div className="points-container">
-          <animated.h1 className="top-of-bottom-fixed" style={divAppearStyle1}>
+          <animated.h1 className="points" style={divAppearStyle1}>
             {divAppearStyle1.text}
           </animated.h1>
-          <animated.h1 className="top-of-bottom-fixed" style={countPointsStyle}>
+          <animated.h1 className="points" style={countPointsStyle}>
             {countPointsStyle.number.interpolate(val => Math.floor(val))}
           </animated.h1>
-          <animated.h1 className="top-of-bottom-fixed" style={divAppearStyle2}>
+          <animated.h1 className="points" style={divAppearStyle2}>
             {divAppearStyle2.text}
           </animated.h1>
         </div>
-        <div className="bot-of-bottom-fixed">
+        <div className="winner">
           <animated.h1
             className="winner-headline"
             style={nameWinnerStyle}
