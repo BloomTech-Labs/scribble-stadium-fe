7bc65bc9ab532af8048391bd979a85bd500902ef
Olivia Toolson <livytoolson@gmail.com>
2021-03-17 10:31:21 -0700

    imported new dev tools, removoed sider, commented out old dev tools for time being

--- src/components/pages/AdminDashboard/index.js
+++ src/components/pages/AdminDashboard/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { Button, Layout, PageHeader } from 'antd';
-// import Sider from 'antd/lib/layout/Sider';
 
 import ModerationTools from './ModerationTools';
-import DevTools from './DevTools';
+// import DevTools from './DevTools';
+import DevToolsNew from './DevToolsNew';
 
 const { Content } = Layout;
 
@@ -18,21 +18,17 @@ const AdminDashboard = props => {
 
   return (
     <Layout className="moderation-page">
-      {/* <Layout> */}
       <PageHeader>
         <h1>Story Squad</h1>
       </PageHeader>
       <Layout>
-        {/* <Sider>
-        Would links to fast scroll to specific sections be useful?
-        </Sider> */}
-
         <Content>
           <Button type="default" onClick={homePageHandler}>
             Back to Home Page
           </Button>
           <ModerationTools component={ModerationTools} />
-          <DevTools component={DevTools} />
+          <DevToolsNew component={DevToolsNew} />
+          {/* <DevTools component={DevTools} /> */}
         </Content>
       </Layout>
     </Layout>
