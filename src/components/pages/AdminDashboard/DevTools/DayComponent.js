a5b6cf5becd79e8c662c3e6c64febeb8863da05a
Olivia Toolson <livytoolson@gmail.com>
2021-03-24 08:11:34 -0700

    updated styling, added more clarifying data from dayData.sj

--- src/components/pages/AdminDashboard/DevTools/DayComponent.js
+++ src/components/pages/AdminDashboard/DevTools/DayComponent.js
@@ -18,37 +18,34 @@ const DayComponent = ({ day }) => {
   //     value: e.target.value,
   // });
 
-  const radioStyle = {
-    display: 'block',
-    height: '50px',
-    size: 'small',
-  };
-
   return (
     <div className="dev-tools-day">
       <h2>{day.dayName}</h2>
       <h3>{day.dayNumber}</h3>
       <h4>{day.stage}</h4>
       <Content>
+        <p>{day.asterik}</p>
         <p>{day.content}</p>
         <p>Select the game state you would like to see in play:</p>
         {/* Add onChange handler in Radio.Group */}
-        <Radio.Group value={gameState}>
-          {day.state.map(choices => {
-            return (
-              <Radio style={radioStyle} value={setGameState}>
-                {choices}
-              </Radio>
-            );
-          })}
-        </Radio.Group>
-        <Button
-          style={{ margin: '1rem' }}
-          onClick={handleSim}
-          disabled={day.gameStageUrl == null}
-        >
-          Simulate Game Play
-        </Button>
+        <div className="state-buttons">
+          <Radio.Group value={gameState}>
+            {day.state.map(choices => {
+              return (
+                <Radio className="radio-buttons" value={setGameState}>
+                  {choices}
+                </Radio>
+              );
+            })}
+          </Radio.Group>
+          <Button
+            className="simulate-button"
+            onClick={handleSim}
+            disabled={day.gameStageUrl == null}
+          >
+            Simulate Game Play
+          </Button>
+        </div>
       </Content>
     </div>
   );
