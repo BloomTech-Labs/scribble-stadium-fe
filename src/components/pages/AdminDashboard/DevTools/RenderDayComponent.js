b8887b6d0f7e47399b5b33fb353a6e29acaabbce
Olivia Toolson <livytoolson@gmail.com>
2021-03-24 17:53:08 -0700

    commented out components we no longer need, will delete after team confirms

--- src/components/pages/AdminDashboard/DevTools/RenderDayComponent.js
+++ src/components/pages/AdminDashboard/DevTools/RenderDayComponent.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import { Layout, Button } from 'antd';
+// import React from 'react';
+// import { useHistory, useParams } from 'react-router-dom';
+// import { Layout, Button } from 'antd';
 
-import { dayData } from './dayData';
-import DayComponent from './DayComponent';
+// import { dayData } from './dayData';
+// import DayComponent from './DayComponent';
 
-const { Header, Footer } = Layout;
+// const { Header, Footer, Content } = Layout;
 
-const RenderDayComponent = props => {
-  const { push } = useHistory();
-  const { dayID } = useParams();
+// const RenderDayComponent = props => {
+//   const { push } = useHistory();
+//   // const { dayID } = useParams();
 
-  const adminDash = () => {
-    push('/admin');
-  };
+//   const adminDash = () => {
+//     push('/admin');
+//   };
 
-  return (
-    <Layout className="dev-ops-page">
-      <Header className="ant-page-header">
-        <h1>Story Squad</h1>
-        <Button onClick={adminDash}>Back to Admin Dashboard</Button>
-      </Header>
-      <div>
-        {dayData.map(day => {
-          if (day.dayID == dayID) {
-            return <DayComponent className="dev-tools-day" day={day} />;
-          }
-        })}
-      </div>
-      <Footer></Footer>
-    </Layout>
-  );
-};
+//   return (
+//     <Layout className="dev-ops-page">
+//       <Header className="ant-page-header">
+//         <h1>Story Squad</h1>
+//         <Button onClick={adminDash}>Back to Admin Dashboard</Button>
+//       </Header>
+//       {/* <div>
+//         {dayData.map(day => {
+//           if (day.dayID == dayID) {
+//             return <DayComponent className="dev-tools-day" day={day} />;
+//           }
+//         })}
+//       </div> */}
+//       <Footer></Footer>
+//     </Layout>
+//   );
+// };
 
-export default RenderDayComponent;
+// export default RenderDayComponent;
