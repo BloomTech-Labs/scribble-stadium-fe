0606ffd96132e3bd79ffba194e79cf284d65e4cf
Yinusa <yinusaamusa137@gmail.com>
2021-03-29 16:11:58 -0500

    implemented devModeHeader

--- src/components/pages/AdminDashboard/devModeHeader.js
+++ src/components/pages/AdminDashboard/devModeHeader.js
@@ -8,11 +8,13 @@ const DevHeader = styled.header`
   background: red;
   text-align: center;
 `;
-
+// This is the DevModeHeader this will be displayed on all pages when DevMode option is toggled
+// One thing to note as more components are added this will have to be updated with all the pages.
+// Along the line there might be an easier method to implement this
 const DevModeHeader = ({ devMode }) => {
   return (
     <div>
-      {devMode.isDevModeActive ? (
+      {devMode.isDevModeActive ? ( //Essentially it check if DevMode option is Active
         <DevHeader> Dev Mode Active</DevHeader>
       ) : (
         console.log('')
