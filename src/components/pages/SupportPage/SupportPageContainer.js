50f1b50f9b5f3637d8e482dadcca9951e8948ff0
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-04-22 18:58:35 -0700

    completed adding buttons on page next step will be to hook them up

--- src/components/pages/SupportPage/SupportPageContainer.js
+++ src/components/pages/SupportPage/SupportPageContainer.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Header } from '../../common';
 
 function SupportPageContainer(props) {
-  return <div>TEST</div>;
+  return (
+    <>
+      <Header title="Support" displayMenu={true} />
+      <div className="container">
+        <button className="FAQ">FAQ</button>
+        <button className="contact">Contact Us</button>
+      </div>
+    </>
+  );
 }
 
 export default SupportPageContainer;
