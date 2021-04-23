35fe7c5b30d7c6b5a005506b6367643f51cf79e9
Mosharraf Musa <mosharrafny@gmail.com>
2021-04-22 22:18:04 -0400

    initial commit setup parent-dash-faq-page

---
+++ src/components/pages/ParentDashFaq/ParentDashFaqContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ParentDashFaqContainer = () => {
+  return (
+    <>
+      <h1>Welcome from ParentDashFaq</h1>
+    </>
+  );
+};
+
+export default ParentDashFaqContainer;
--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ import { MissionControl } from './components/pages/MissionControl';
 import { Modal } from './components/pages/Modal';
 import { NotFoundPage } from './components/pages/NotFound';
 import { ParentDashboard } from './components/pages/ParentDashboard';
+import { ParentDashFaq } from './components/pages/ParentDashFaq';
 import { ParentSettings } from './components/pages/FamilySettings';
 import { StoryPrompt } from './components/pages/StoryPrompt';
 import { WritingSub } from './components/pages/WritingSub';
@@ -60,6 +61,7 @@ import Wed from './components/pages/AdminDashboard/DevTools/DayComponents/05_Wed
 import Thurs from './components/pages/AdminDashboard/DevTools/DayComponents/06_Thurs';
 import Fri from './components/pages/AdminDashboard/DevTools/DayComponents/07_Fri';
 import DevModeHeader from './components/pages/AdminDashboard/devModeHeader';
+import { ParentDashboardFaq } from './components/pages/ParentDashFaq';
 
 // import RenderDayComponent from './components/pages/AdminDashboard/DevTools/RenderDayComponent.js';
 
@@ -146,6 +148,13 @@ function App() {
           exact
           component={() => (
             <ParentDashboard LoadingComponent={ParentLoadingComponent} />
+          )}
+        />
+        <SecureRoute
+          path="/parent/dashboard-faq"
+          exact
+          component={() => (
+            <ParentDashFaq LoadingComponent={ParentLoadingComponent} />
           )}
         />
         <SecureRoute
