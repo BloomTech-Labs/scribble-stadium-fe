00787ec023891da7c84ef59ca06a939f7558a194
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-04-23 13:38:52 -0700

    added packages to handle contact form functionality

--- src/components/pages/SupportPage/SupportPageContainer.js
+++ src/components/pages/SupportPage/SupportPageContainer.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from '../../common';
+import { Modal, Button } from 'antd';
 
 function SupportPageContainer(props) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  // const SupportModal = props => {
+  //   // const {push} = useHistory();
+  //   const {
+  //     modalVisible,
+  //     style,
+  //     instructions,
+  //     showOkButton = false,
+  //     handleCancel,
+  //     handleOk,
+  //   } = props;
+
   return (
     <>
       <Header title="Support" displayMenu={true} />
+
+      <Modal
+        className="Contact-modal"
+        visible={modalVisible}
+        keyboard={true}
+        width={'70%'}
+        onCancel={() => setModalVisible(false)}
+        zIndex={2000}
+        //onOk={handleOk}
+        cancelButtonProps={{ disabled: true }}
+        footer="Submit"
+        closeIcon="X"
+      >
+        <p>Contact Us</p>
+
+        {/* {showOkButton && (
+          <Button onClick={handleCancel} className="send-button">
+            I Accept!!!
+          </Button>
+        )} */}
+      </Modal>
+
       <div className="container">
-        <button className="FAQ">FAQ</button>
-        <button className="contact">Contact Us</button>
+        <button className="FAQ">
+          <h3>FAQ</h3>
+        </button>
+        <button className="contact" onClick={() => setModalVisible(true)}>
+          <h3>Contact Us</h3>
+        </button>
       </div>
     </>
   );
