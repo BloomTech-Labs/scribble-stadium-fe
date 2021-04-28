26a598ae7af2acf1cf00aa0cf5aa6ee515a5c18e
John Youn <john@Johns-MacBook-Pro.local>
2021-04-27 20:01:05 -0700

    added a rank column and moved total points column to the end

--- src/components/pages/TrophyRoom/Leaderboard.js
+++ src/components/pages/TrophyRoom/Leaderboard.js
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Table } from 'antd';
 import { getLeaderboard } from '../../../api';
 import { useOktaAuth } from '@okta/okta-react/dist/OktaContext';
-
 const Leaderboard = () => {
   const { authState } = useOktaAuth();
   const [data, setDataInfo] = useState([]);
-
   useEffect(() => {
     //Getting data from backend for leaderboard
     getLeaderboard(authState).then(res => {
@@ -14,21 +12,37 @@ const Leaderboard = () => {
     });
   }, [authState]);
 
-  // Testing for ranking/ placements for the table
-
-  // let index = [];
-  // for (let i = 0; i <= data.length; i++) {
-  //   index.push({ key: i, index: i });
-  // }
-
   const table = [
     // Structre of the Table
-
     //Person is still able to sort data even with sorter tooltip set to false
     //Might not be worth using ant Table and just using colomns and Rows since you have more flexiblity and
     // more freedom to do what you want and set up data the way youu want it to be displayed
-    // Add Rankings to the table
 
+    {
+      title: 'Rank',
+      key: 'Rank',
+      render: (text, record, index) => index + 1,
+      width: 100,
+      defaultSortOrder: 'descend',
+    },
+    {
+      title: '',
+      dataIndex: 'Name',
+      render: () => (
+        <img
+          src="https://labs28-b-storysquad.s3.amazonaws.com/hero-3.svg"
+          alt="avatar"
+        />
+      ),
+      key: 'Name',
+      width: 150,
+    },
+    {
+      title: 'Name',
+      dataIndex: 'Name',
+      key: 'Name',
+      width: 150,
+    },
     {
       title: 'Wins',
       dataIndex: 'Wins',
@@ -43,19 +57,6 @@ const Leaderboard = () => {
       dataIndex: 'Losses',
       width: 100,
     },
-
-    {
-      title: 'Name',
-      dataIndex: 'Name',
-      key: 'Name',
-      width: 150,
-    },
-    {
-      title: 'Total Points',
-      dataIndex: 'Total_Points',
-      key: 'Total_points',
-      width: 100,
-    },
     {
       title: 'Writing Points',
       dataIndex: 'WritingPoints',
@@ -68,8 +69,13 @@ const Leaderboard = () => {
       key: 'DrawingPoints',
       width: 100,
     },
+    {
+      title: 'Total Points',
+      dataIndex: 'Total_Points',
+      key: 'Total_points',
+      width: 100,
+    },
   ];
-
   return (
     <div className="leaderboard">
       <h2 className="h2">Leaderboard</h2>
@@ -82,5 +88,4 @@ const Leaderboard = () => {
     </div>
   );
 };
-
 export default Leaderboard;
