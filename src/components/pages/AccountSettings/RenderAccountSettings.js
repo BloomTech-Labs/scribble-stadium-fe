378661efd56c6fb9dfaacbc8cd475c1cc8e99509
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-05-09 12:33:08 -0700

    completed button toggle functionality

--- src/components/pages/AccountSettings/RenderAccountSettings.js
+++ src/components/pages/AccountSettings/RenderAccountSettings.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function RenderAccountSettings(props) {
-  return <div style={{ border: '20px solid red', height: '200px' }}>TEST</div>;
+  const [unlock, setUnlock] = useState(true);
+  console.log(unlock);
+  return (
+    <div className="accountSettingsContainer">
+      <div className="editText">
+        <h3>Edit Account Settings</h3>
+      </div>
+      <div className="unlockButton" style={unlock ? null : { display: 'none' }}>
+        <button onClick={() => setUnlock(false)}>UNLOCK</button>
+      </div>
+      <div className="lockButton" style={unlock ? { display: 'none' } : null}>
+        <button onClick={() => setUnlock(true)}>LOCK</button>
+      </div>
+    </div>
+  );
 }
 
 export default RenderAccountSettings;
