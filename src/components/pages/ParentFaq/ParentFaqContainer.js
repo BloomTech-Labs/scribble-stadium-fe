46f652e5eda261c2beee131adfa28212be961fa9
Mosharraf Musa <mosharrafny@gmail.com>
2021-05-09 13:31:15 -0400

    parent-faq-page setup and router setting

---
+++ src/components/pages/ParentFaq/ParentFaqContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Layout } from 'antd';
+import ParentNavTopBar from '../../common/ParentNavTopBar';
+
+const ParentFaqContainer = () => {
+  return (
+    <>
+      <Layout className="parent-dashboard">
+        <ParentNavTopBar />
+      </Layout>
+    </>
+  );
+};
+
+export default ParentFaqContainer;
