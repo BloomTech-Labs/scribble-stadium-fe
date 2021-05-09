f8901d669168e4bf9056121a2d08717c97547b85
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-05-09 12:44:23 -0700

    changed opacity of container depending on boolean

--- src/components/pages/AccountSettings/RenderAccountSettings.js
+++ src/components/pages/AccountSettings/RenderAccountSettings.js
@@ -2,17 +2,44 @@ import React, { useState } from 'react';
 
 function RenderAccountSettings(props) {
   const [unlock, setUnlock] = useState(true);
-  console.log(unlock);
+
   return (
     <div className="accountSettingsContainer">
-      <div className="editText">
-        <h3>Edit Account Settings</h3>
+      <div className="textAndButtonContainer">
+        <div className="editText">
+          <h3>Edit Account Settings</h3>
+        </div>
+        <div
+          className="unlockButton"
+          style={unlock ? null : { display: 'none' }}
+        >
+          <button onClick={() => setUnlock(false)}>UNLOCK</button>
+        </div>
+
+        <div className="lockButton" style={unlock ? { display: 'none' } : null}>
+          <button onClick={() => setUnlock(true)}>LOCK</button>
+        </div>
       </div>
-      <div className="unlockButton" style={unlock ? null : { display: 'none' }}>
-        <button onClick={() => setUnlock(false)}>UNLOCK</button>
-      </div>
-      <div className="lockButton" style={unlock ? { display: 'none' } : null}>
-        <button onClick={() => setUnlock(true)}>LOCK</button>
+      <div
+        className="editFormsAndButtonsContainer"
+        style={unlock ? { opacity: '.3' } : null}
+      >
+        <div className="placeholderInput">
+          <input placeholder="INPUT"></input>
+          <button>SUBMIT</button>
+        </div>
+        <div className="placeholderInput">
+          <input placeholder="INPUT"></input>
+          <button>SUBMIT</button>
+        </div>
+        <div className="placeholderInput">
+          <input placeholder="INPUT"></input>
+          <button>SUBMIT</button>
+        </div>
+        <div className="placeholderInput">
+          <input placeholder="INPUT"></input>
+          <button>SUBMIT</button>
+        </div>
       </div>
     </div>
   );
