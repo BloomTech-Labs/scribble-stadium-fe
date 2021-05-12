3d5e0c7ae5d79bbddd83be84562ac27335ecfb51
Tucker <leachtucker@gmail.com>
2021-05-11 19:18:59 -0700

    Removes old code. Integrates new components

--- src/components/pages/EditPlayers/RenderEditPlayers.js
+++ src/components/pages/EditPlayers/RenderEditPlayers.js
@@ -6,27 +6,12 @@ import { Layout, Form, Input, Button, Typography } from 'antd';
 import { PlusCircleOutlined } from '@ant-design/icons';
 
 import ParentNavTopBar from '../../common/ParentNavTopBar';
-import ChildCard from '../../common/ChildCard';
+import ChildForm from '../../common/ChildForm';
 
 const { Title } = Typography;
 
-const layoutSettings = {
-  labelCol: { span: 12 },
-  wrapperCol: { span: 16 },
-};
-
 const EditPlayers = props => {
   const { push } = useHistory();
-
-  const handleChange = (value, e) => {
-    console.log('this is value', value);
-  };
-
-  const [form] = Form.useForm();
-
-  const onFinish = values => {
-    push('/parent-dashboard');
-  };
 
   return (
     <Layout className="parent-dashboard edit-players">
@@ -38,13 +23,9 @@ const EditPlayers = props => {
           </Title>
           <Button icon={<PlusCircleOutlined />}>Add Player</Button>
         </div>
-        <Layout className="children" style={{ flexFlow: 'row wrap' }}>
+        <Layout className="children">
           {props.parent.children.map(child => (
-            <ChildCard
-              name={child.Name}
-              AvatarURL={child.AvatarURL}
-              update="SETTINGS"
-            />
+            <ChildForm name={child.Name} AvatarURL={child.AvatarURL} />
           ))}
         </Layout>
       </Layout>
