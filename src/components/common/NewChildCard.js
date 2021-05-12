65178257738dfaa80af77662ab575b347bb62d32
charlene-johnson <charlene.j8234@gmail.com>
2021-05-11 22:33:38 -0400

    started styling the child card

--- src/components/common/NewChildCard.js
+++ src/components/common/NewChildCard.js
@@ -7,20 +7,28 @@ export default function NewChildCard(props) {
   console.log('This is on the NewChildCard component', props.props);
   return (
     <div>
-      <div className="children">
+      <div className="newChild">
         {props.props.parent.children.map((child, i) => (
           <Card>
             <div className="childrenContainer">
-              <img src={child.AvatarURL} alt="avatar"/>
-              <h3>{child.Name}</h3>
-              <h4>Cohort #</h4>
+              {/* This is hard coded right now, once data is ready, can replace it */}
+              <h4>Status: Matchup</h4>
+              <div className="avatarContainer">
+                <div className="avatarbackground">
+                  <img src={child.AvatarURL} alt="avatar"/>  
+                </div>
+                <h2>{child.Name}</h2>
+              </div>
+              <div className="stats">
               <p>{child.CohortID}</p>
-              <h4>Total Pts</h4>
+              <h4>Week #</h4>
               <p>Total Points Here</p>
-              <h4>Wins</h4>
+              <h4>Total Pts</h4>
               <p>{child.Wins}</p>
-              <h4>Losses</h4>
+              <h4>Wins</h4>
               <p>{child.Losses}</p>
+              <h4>Losses</h4>
+              </div>
             </div>
           </Card>
         ))}
@@ -34,3 +42,4 @@ export default function NewChildCard(props) {
 // Avatar situated to the left bottom of the card
 // from top to bottom "status: matchup, mission accomplished, etc" , week# , total points, wins, losses
 //AvatarURL, CohortID, GradeLevel, ID, IsDyslexic, Name, PIN, ParentID, type: "Child"
+// "status" - matchup, i story , in drawing,
