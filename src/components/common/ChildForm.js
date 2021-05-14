2a3f060e22f300ba01604cdb546b659308856d8b
Tucker <leachtucker@gmail.com>
2021-05-13 18:20:17 -0700

    Lays out the basic functionality of the ChildForm component

--- src/components/common/ChildForm.js
+++ src/components/common/ChildForm.js
@@ -1,58 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Button, Card, Form, Input } from 'antd';
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
 function ChildForm(props) {
   const [form] = Form.useForm();
+  const [isSaveBtnDisabled, setIsSaveBtnDisabled] = useState(() => true);
+
+  const onChange = () => {
+    if (isSaveBtnDisabled) {
+      setIsSaveBtnDisabled(() => false);
+    }
+  };
 
   const onFinish = values => {
+    console.log('Submitted:');
     console.log(values);
   };
 
   return (
     <Card className="child-card-form" bordered={false}>
       <div className="card-top">
-        <div>
+        <div className="avatar-container">
           <img src={props.AvatarURL} alt="avatar" className="avatar-img" />
-          <Button icon={<EditOutlined />} className="edit-btn">
-            Edit
-          </Button>
+          {/* This button allows parents to change their kid's avatar */}
+          <Button icon={<EditOutlined />} className="edit-btn" />
         </div>
         <Button icon={<DeleteOutlined />} className="delete-btn">
           Delete
         </Button>
       </div>
-      <div>
-        <Form form={form} name="control-hooks" onFinish={onFinish}>
-          <div>
-            <Form.Item
-              name="characterName"
-              label="Character Name"
-              rules={[{ required: true }]}
-            >
+      <div className="card-center">
+        <Form
+          form={form}
+          initialValues={{
+            name: props.Name,
+            characterName: 'Pinky Winky',
+            pin: '0000',
+            email: 'placeholder@maildrop.cc',
+          }}
+          name="control-hooks"
+          layout="inline"
+          onChange={onChange}
+          onFinish={onFinish}
+        >
+          <div className="col">
+            <Form.Item name="characterName" label="Character Name">
               <Input />
             </Form.Item>
-            <Form.Item name="pin" label="PIN" rules={[{ required: true }]}>
+            <Form.Item name="pin" label="PIN">
+              <Input type="password" className="pin" />
+            </Form.Item>
+          </div>
+          <div className="col">
+            <Form.Item name="name" label="Name">
+              <Input />
+            </Form.Item>
+            <Form.Item name="email" label="Email Address">
               <Input />
             </Form.Item>
           </div>
-          <div>
-            <Form.Item name="name" label="Name" rules={[{ required: true }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name="email"
-              label="Email Address"
-              rules={[{ required: true }]}
+          <div className="card-bottom">
+            <Button
+              htmlType="submit"
+              className="save-btn"
+              disabled={isSaveBtnDisabled}
             >
-              <Input />
-            </Form.Item>
+              Save Changes
+            </Button>
           </div>
         </Form>
-      </div>
-      <div>
-        <Button className="save-btn">Save Changes</Button>
       </div>
     </Card>
   );
