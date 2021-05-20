9de8a69c1facaf6fea8bd4770d835000be7ae884
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-05-20 02:28:04 -0700

    Need to make it go back and forth

--- src/components/pages/Gamemode/Gamemode.js
+++ src/components/pages/Gamemode/Gamemode.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import history from './history';
+// import history from './history';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { Link, Router, Route, Switch } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import GamemodeButton from './GamemodeButton';
 import { render } from 'react-dom';
 
 const Gamemode = () => {
-  const { push } = useHistory();
+  const { push, location } = useHistory();
   //   history.push('/gamemode/single');
   const singled = () => {
     push('/gamemode/single');
   };
 
-  console.log(history);
+  //   console.log(history);
   return (
-    <Switch>
-      <Router history={history}>
-        <Link to="/gamemode/single">
-          <button onClick={singled}> Single Player</button>
-          <Route path="/gamemode/single" component={GamemodeButton} />
-        </Link>
-      </Router>
-    </Switch>
+    <Link to="/gamemode/single">
+      <button onClick={singled}> Single Player</button>
+      <Route path="/gamemode/single" component={GamemodeButton} />
+    </Link>
   );
 };
 export default connect()(Gamemode);
