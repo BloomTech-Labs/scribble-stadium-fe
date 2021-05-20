44e8caa4cd1ca77354ac61ddf78c5c0aa3b2d084
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-05-20 01:36:47 -0700

    Using props history

--- src/components/pages/Gamemode/Gamemode.js
+++ src/components/pages/Gamemode/Gamemode.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import history from './history';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Link, Router, Route, Switch } from 'react-router-dom';
 import GamemodeButton from './GamemodeButton';
 import { render } from 'react-dom';
 
-const singlePlay = () => {
-  history.push('/gamemode/single');
+const Gamemode = () => {
+  const { push } = useHistory();
+  //   history.push('/gamemode/single');
+  const singled = () => {
+    push('/gamemode/single');
+  };
 
   console.log(history);
+  return (
+    <Switch>
+      <Router history={history}>
+        <Link to="/gamemode/single">
+          <button onClick={singled}> Single Player</button>
+          <Route path="/gamemode/single" component={GamemodeButton} />
+        </Link>
+      </Router>
+    </Switch>
+  );
 };
-
-export default class Gamemode extends Component {
-  render() {
-    return (
-      <Switch>
-        <Router history={history}>
-          <Link to="/gamemode/single">
-            <button onClick={singlePlay}> Single Player</button>
-            <Route path="/gamemode/single" component={GamemodeButton} />
-          </Link>
-        </Router>
-      </Switch>
-    );
-  }
-}
-
+export default connect()(Gamemode);
 // export default connect()(Gamemode);
