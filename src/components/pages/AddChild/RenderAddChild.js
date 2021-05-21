4803fdc7a5d60c4a42b1f2de62c9c9852d41d189
Tucker <leachtucker@gmail.com>
2021-05-20 17:19:03 -0700

    Removed some defunct code! Cleaning up

--- src/components/pages/AddChild/RenderAddChild.js
+++ src/components/pages/AddChild/RenderAddChild.js
@@ -1,67 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { useOktaAuth } from '@okta/okta-react';
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { Layout, Typography } from 'antd';
 
 import ParentNavTopBar from '../../common/ParentNavTopBar';
 import ParentDashboardBack from '../../common/ParentDashboardBack';
 import ChildForm from '../../common/ChildForm';
 
-import { useHistory } from 'react-router-dom';
-import { getChildFormValues } from '../../../api';
-import { postNewChild, getChild } from '../../../api';
-
-import { Layout, Form, Input, Button, Select, Switch, Typography } from 'antd';
-
-import { connect } from 'react-redux';
-import ImagePicker from 'react-image-picker';
-import { PlusOutlined } from '@ant-design/icons';
-import Modal from 'antd/lib/modal/Modal';
-
 const { Title } = Typography;
-const { Option } = Select;
 
 const RenderAddChild = props => {
-  const { authState } = useOktaAuth();
-
-  const [gradeLevels, setGradeLevels] = useState([]);
-  const [avatars, setAvatars] = useState([]);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [form] = Form.useForm();
-  const { push } = useHistory();
-
-  useEffect(() => {
-    getChildFormValues(authState).then(data => {
-      setAvatars(() => data[0]);
-      setGradeLevels(() => data[1]);
-    });
-  }, [authState]);
-
-  const pickHandler = image => {
-    setSelectedImage(image);
-  };
-
-  /**
-   *
-   * @param {Object} values fields including Name, PIN, IsDyslexic, CohortID, ParentID, AvatarID, and GradeLevelID
-   * @returns {number} the newly created child id is put into the getChild api call
-   */
-  const onFinish = ({ Name, PIN, GradeLevelID, IsDyslexic }) => {
-    postNewChild(authState, {
-      Name,
-      PIN,
-      GradeLevelID,
-      IsDyslexic,
-      AvatarID: selectedImage.value,
-      ParentID: props.parent.id,
-      CohortID: 1,
-    }).then(res => {
-      getChild(authState, res).then(child => {
-        props.setChildren({ ...child });
-      });
-    });
-    push('/parent/dashboard');
-  };
-
   return (
     <Layout className="add-child">
       <ParentNavTopBar />
