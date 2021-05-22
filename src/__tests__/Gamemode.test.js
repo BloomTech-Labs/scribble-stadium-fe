1f1f72d67a881d43d1f3bbf3289995afc1c131d8
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-05-22 08:16:19 -0700

    prop tests

---
+++ src/__tests__/Gamemode.test.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import { render, cleanup } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { Gamemode } from '../components/pages/Gamemode';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { tasks, global } from '../state/actions';
+import { reducer } from '../state/reducers/taskReducer';
+
+describe('history mock', () => {
+  it('should configure a history mock from dom react', () => {
+    configure({ adapter: new Adapter() });
+
+    jest.mock('react-router-dom', () => ({
+      useHistory: () => ({
+        push: jest.fn(),
+      }),
+    }));
+  });
+});
+
+describe('taskReducer test suite', () => {
+  const initialState = {
+    id: null,
+    child_id: null,
+    child: {
+      id: null,
+      name: null,
+      isDyslexic: null,
+      avatarUrl: null,
+      gamemode: null,
+      gradeLevel: null,
+      parentId: null,
+      cohortId: null,
+      memberId: null,
+      VotesRemaining: null,
+      totalPoints: null,
+      wins: null,
+      losses: null,
+      achievements: null,
+      Ballots: [],
+      Streaks: '',
+    },
+    story_id: null,
+    hasRead: false,
+    hasWritten: false,
+    hasDrawn: false,
+    complexity: null,
+    LowConfidence: null,
+    story: {
+      drawingPrompt: '',
+      writingPrompt: '',
+      storyTitle: '',
+      storyUrl: null,
+    },
+  };
+
+  it('should return changes to the state', () => {
+    const action = {
+      type: tasks.SET_TASKS,
+      payload: {
+        ID: 1,
+        ChildID: 1,
+        child: {
+          id: null,
+          name: null,
+          isDyslexic: null,
+          avatarUrl: null,
+          gamemode: null,
+          gradeLevel: null,
+          parentId: null,
+          cohortId: null,
+          memberId: null,
+          VotesRemaining: null,
+          totalPoints: null,
+          wins: null,
+          losses: null,
+          achievements: null,
+          Ballots: [],
+          Streaks: '',
+        },
+        StoryID: 1,
+        HasRead: false,
+        HasWritten: false,
+        HasDrawn: false,
+        Complexity: false,
+      },
+    };
+    const state = reducer(initialState, action);
+    expect(state).toEqual({
+      ...initialState,
+      id: 1,
+      child_id: 1,
+      child: {
+        id: null,
+        name: null,
+        isDyslexic: null,
+        avatarUrl: null,
+        gamemode: null,
+        gradeLevel: null,
+        parentId: null,
+        cohortId: null,
+        memberId: null,
+        VotesRemaining: null,
+        totalPoints: null,
+        wins: null,
+        losses: null,
+        achievements: null,
+        Ballots: [],
+        Streaks: '',
+      },
+      story_id: 1,
+      hasRead: false,
+      hasWritten: false,
+      hasDrawn: false,
+      complexity: false,
+    });
+  });
+
+  it('should return initialstate with value of hasRead true', () => {
+    const action = { type: tasks.SET_HAS_READ };
+    const state = reducer(initialState, action);
+    expect(state).toEqual({ ...initialState, hasRead: true });
+  });
+
+  it('should return initialstate with value of hasWrtten true', () => {
+    const action = { type: tasks.SET_HAS_WRITTEN };
+    const state = reducer(initialState, action);
+    expect(state).toEqual({ ...initialState, hasWritten: true });
+  });
+
+  it('should return initialstate with value of hasDrawn true', () => {
+    const action = { type: tasks.SET_HAS_DRAWN };
+    const state = reducer(initialState, action);
+    expect(state).toEqual({ ...initialState, hasDrawn: true });
+  });
+
+  it('should return initialstate with submissioninformation filled out', () => {
+    const action = {
+      type: tasks.SET_SUBMISSION_INFORMATION,
+      payload: {
+        ...initialState,
+        DrawingPrompt: 'drawing',
+        WritingPrompt: 'writing',
+        Title: 'title',
+        URL: 'storyurl',
+      },
+    };
+    const state = reducer(initialState, action);
+    expect(state).toEqual({
+      ...initialState,
+      story: {
+        drawingPrompt: 'drawing',
+        writingPrompt: 'writing',
+        storyTitle: 'title',
+        storyUrl: 'storyurl',
+      },
+    });
+  });
+});
