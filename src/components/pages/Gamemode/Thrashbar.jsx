1aa9dc01536a5b45f67de33d2c9ca8a16e75528d
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-06-01 13:44:28 -0700

    button onclick opacity state jsx works

--- src/components/pages/Gamemode/Thrashbar.jsx
+++ src/components/pages/Gamemode/Thrashbar.jsx
@@ -51,7 +51,7 @@ componentDidUpdate(prevProps,prevState) {
  if (this.state.child.gamemode.write !== prevState.child.gamemode.write) {
      prevState.child.gamemode.write = this.state.child.gamemode.write;
    console.log('prevstate',prevState.child.gamemode.write+'prevethis'+this.props.child.gamemode.write);
-   // this.forceUpdate();
+   
  }
 // Draw
  if (this.props.child.gamemode.draw !== prevProps.child.gamemode.draw) {
@@ -63,87 +63,15 @@ componentDidUpdate(prevProps,prevState) {
 if (this.state.child.gamemode.draw !== prevState.child.gamemode.draw) {
    prevState.child.gamemode.draw = this.state.child.gamemode.draw;
  console.log('prevstate',prevState.child.gamemode.draw+'prevethis'+this.props.child.gamemode.draw);
- // this.forceUpdate();
+
 }
 }
-
-// componentDidMount() {
-//   console.log('inthrash');
-//   if (
-    
-//     this.props.child.gamemode.read != null  
-//   ) {
-//     console.log('inthrash',this.state.child.gamemode.read);
-//     this.setState({ child:{
-//       gamemode:{
-//         mode: 'single',
-//         read: this.props.child.gamemode.read,
-//         write: false,
-//         draw: false,
-//         sp: true
-//       }
-//     }  });
-//     console.log('inthrash',this.state.child.gamemode.read);
-//   }else{
-//     console.log('inthrash');
-//     this.setState( { child:{
-//       gamemode:{
-//         mode: 'single',
-//         read: false,
-//         write: false,
-//         draw: false,
-//         sp: true
-//       }
-//     }  });
-//   }
-//   // at this point you can query the browser DOM
-//   // (even tho most of the time you DONT want to do this)
-//   // componentdidmount 
-// }
-
-
-
-// componentDidUpdate(prevProps, prevState) {
-//   // similar to useEffect
-//   // we have to manually check for certain props or state changes
-//   // no dependency array in cDU :(
-//     console.log('inthrash');
-//   if (
-    
-//     prevProps.child.gamemode !== this.state.child.gamemode
-//   ) {
-//     console.log('inthrash');
-//     this.setState({ child:{
-//       gamemode:{
-//         mode: 'single',
-//         read: false,
-//         write: false,
-//         draw: false,
-//         sp: true
-//       }
-//     }  });
-//   }else{
-//     console.log('inthrash');
-//     // this.setState( { child:{
-//     //   gamemode:{
-//     //     mode: null,
-//     //     read: null,
-//     //     write: null,
-//     //     draw: null,
-//     //     sp: null
-//     //   }
-//     // }  });
-//   }
-// }
 
 
    singled(e){
 
       this.props.sread(e);
-    
      
-     
-
   };
 render(){
   return (
@@ -171,11 +99,6 @@ render(){
                     this.singled(e) ;
                 
                   }}
-              
-                  
-                
-                
-
                  id="mission-read-button" >1</button>
                 <p className="read-button-font">Read</p>
               </Col>
