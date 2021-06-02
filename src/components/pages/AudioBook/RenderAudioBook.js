0266abefd0300005c6f84b401eeda0f5a34c69fd
Samantha Goodman <antashma@gmail.com>
2021-06-02 19:56:22 -0400

    imported antd, rhap, and custom style sheets

--- src/components/pages/AudioBook/RenderAudioBook.js
+++ src/components/pages/AudioBook/RenderAudioBook.js
@@ -4,14 +4,16 @@ import { Row, Col } from 'antd';
 import { useHistory } from 'react-router-dom';
 import { InstructionsModal } from '../../common';
 import { modalInstructions } from '../../../utils/helpers';
-
-import ReactAudioPlayer from 'react-audio-player';
+import ReactAudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import 'antd/dist/antd.css';
+import './h5audiostyle.css';
 
 const RenderAudioBookContainer = () => {
   return (
     <>
       <Header displayMenu={true} />
-      <ReactAudioPlayer controls />
+      <ReactAudioPlayer />
     </>
   );
 };
