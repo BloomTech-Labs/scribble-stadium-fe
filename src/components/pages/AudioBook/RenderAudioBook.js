98651c8ce9674a07f1ad41be6b26b3f96d639e5f
Samantha Goodman <antashma@gmail.com>
2021-06-02 20:29:55 -0400

    added new audio player component. also imported and used antd icons

--- src/components/pages/AudioBook/RenderAudioBook.js
+++ src/components/pages/AudioBook/RenderAudioBook.js
@@ -5,15 +5,26 @@ import { useHistory } from 'react-router-dom';
 import { InstructionsModal } from '../../common';
 import { modalInstructions } from '../../../utils/helpers';
 import ReactAudioPlayer from 'react-h5-audio-player';
+import { PlayCircleOutlined, PauseCircleOutlined } from '@ant-design/icons';
 import 'react-h5-audio-player/lib/styles.css';
 import 'antd/dist/antd.css';
-import './h5audiostyle.css';
+import './rhap-style-override.css';
 
 const RenderAudioBookContainer = () => {
   return (
     <>
       <Header displayMenu={true} />
-      <ReactAudioPlayer />
+      <ReactAudioPlayer
+        //src={soundfile}
+        style={{ width: '60%' }}
+        customAdditionalControls={[]} //removes loop
+        customVolumeControls={[]} //removes volume
+        customIcons={{
+          //insert antd icons here for each control
+          play: <PlayCircleOutlined />,
+          pause: <PauseCircleOutlined />,
+        }}
+      />
     </>
   );
 };
