d44c50a2fa52a0c265f911ad21470cb39aac3950
Bradley <buddyrad216@gmail.com>
2021-06-17 20:08:29 -0400

    changed the name of the file that I am working on to notification language. will continue to add to this file after the merge

--- src/components/notifications/duedate.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function DueDate(props) {
-  return (
-    <h2>"UserName, you have numberofdays to complete your assignments."</h2>
-  );
-}
-
-export default DueDate;
---
+++ src/components/notifications/notificationlanguage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function DueDate(props) {
+  return (
+    <h2>"UserName, you have numberofdays to complete your assignments."</h2>
+  );
+}
+
+function NewTasks(props) {
+  return (
+    <h2>
+      "Welcome username! You have # of activities remaining to unlock your next
+      adventure! You have # days to complete your activities!"
+    </h2>
+  );
+}
+
+export default DueDate;
