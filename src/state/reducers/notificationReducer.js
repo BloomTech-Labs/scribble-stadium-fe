3ab645675e09b4ace225618889696111c9592455
Amanda Nelson <amanda.gaylord@gmail.com>
2021-07-05 21:29:23 -0400

    initialState values entered from the Notifications migration on the backend.

--- src/state/reducers/notificationReducer.js
+++ src/state/reducers/notificationReducer.js
@@ -1,6 +1,15 @@
 import { notifications } from '../actions/index';
 
-const initialState = {};
+const initialState = {
+  ID: null,
+  Text: null,
+  Type: null,
+  LinksTo: null,
+  Date: date.now(),
+  DueDate: null,
+  ChildID: null,
+  ParentID: null,
+};
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
