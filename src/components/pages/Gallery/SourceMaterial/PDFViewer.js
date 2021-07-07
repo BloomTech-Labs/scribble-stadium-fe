eac1b6509fd2742f5831f6e528e33ef62aff0c29
Ani Ta <aniigarris@gmail.com>
2021-07-06 20:59:28 -0600

    Passed in props. Created and commented out real data until the source error is resolved. Dummy data is currently displaying.

--- src/components/pages/Gallery/SourceMaterial/PDFViewer.js
+++ src/components/pages/Gallery/SourceMaterial/PDFViewer.js
@@ -6,6 +6,8 @@ import { Document, Page, pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PDFViewer = props => {
+  console.log('pdfviewer props', props);
+
   const [scale, setScale] = useState(1.0);
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
@@ -21,21 +23,29 @@ const PDFViewer = props => {
   return (
     <div className="pdf-container">
       <div>
-        <h3>Week's Story </h3>
+        <h3>Week {props.sprint} Story </h3>
       </div>
       <div className="pdf-viewer">
         <PDFLoading isLoading={isLoading} />
         <Document
           className="pdf-components"
+          // Dummy data being displays while AWS data is broken
           file="/assets/docs/sample.pdf"
+          // Real data will be displayed with props when available
+          // file={props.sprintstory}
           onLoadSuccess={onDocumentLoadSuccess}
         >
           <Page pageNumber={pageNumber} scale={scale} />
         </Document>
         <div className="pdf-link">
+          {/* Dummy data being displays while AWS data is broken */}
           <a href="/assets/docs/sample.pdf" download target="_blank">
             Download Story
           </a>
+          {/* Real data will be displayed with props when available */}
+          {/* <a href={props.sprintstory} download target="_blank">
+            Download Story
+          </a> */}
         </div>
         <div className="pdf-components">
           <ControlPanel
