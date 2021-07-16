341076e71003e35547e40806e73f05e41f0bb059
ChadDiaz <chadcdiaz@gmail.com>
2021-07-15 21:48:39 -0400

    imported useOktaAuth directory in the WeeklySubmission.js file as it was missing

--- src/components/pages/Gallery/WeeklySubmissions.js
+++ src/components/pages/Gallery/WeeklySubmissions.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { useOktaAuth } from '@okta/okta-react';
 import { connect } from 'react-redux';
 import Weekly from './Weekly';
 
-const WeeklySubmissions = (props) => {
+const WeeklySubmissions = props => {
   const { authState } = useOktaAuth();
-  
+
   // Passing state to Week Card. Displaying Week card in descending order.
   return (
     <>
       {props.data.reverse().map((child, i) => {
         return (
-          <Weekly key={i}
+          <Weekly
+            key={i}
             // childId={child.ID}
             sprint={child.sprint}
             // galleryId={child.GalleryId}
@@ -20,7 +22,9 @@ const WeeklySubmissions = (props) => {
             writing={child.WritingUrl}
             pagenum={child.PageNum}
             drawing={child.DrawingUrl}
-           />)})}
+          />
+        );
+      })}
     </>
   );
 };
