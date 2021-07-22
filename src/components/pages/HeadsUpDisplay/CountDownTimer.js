54ee07e39cd7a55469117eba8ee447253f7412bc
ngwatso <ngwatson79@yahoo.com>
2021-07-22 11:06:39 -0400

    created variable to calculate current time

--- src/components/pages/HeadsUpDisplay/CountDownTimer.js
+++ src/components/pages/HeadsUpDisplay/CountDownTimer.js
@@ -1,1 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+export const CountDownTimer = () => {
+  // This function will calculate difference between current and target times
+  const calculateTimeRemaining = () => {
+    let currTime = new Intl.DateTimeFormat('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    }).format(Date.now());
+    console.log('current time is ', time);
+    // let timeDiff =
+  };
+
+  const time = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+
+  const [timeRemaining, setTimeRemaining] = useState(time);
+};
