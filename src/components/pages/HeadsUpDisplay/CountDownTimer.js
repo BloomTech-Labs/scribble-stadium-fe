a28211ddb0b950f843cce065e5ac1c98247f37a3
ngwatso <ngwatson79@yahoo.com>
2021-07-23 20:51:23 -0400

    reformatted countdown timer, functioning with static data input

--- src/components/pages/HeadsUpDisplay/CountDownTimer.js
+++ src/components/pages/HeadsUpDisplay/CountDownTimer.js
@@ -1,22 +1,49 @@
 import React, { useState, useEffect } from 'react';
 
 export const CountDownTimer = () => {
-  // This function will calculate difference between current and target times
-  const calculateTimeRemaining = () => {
-    let currTime = new Intl.DateTimeFormat('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    }).format(Date.now());
-    console.log('current time is ', time);
-    // let timeDiff =
+  // This function will convert timer from seconds to HH:MM:SS format
+  const convertTimeFormat = time => {
+    let timeVar = parseInt(time, 10);
+
+    let hours = Math.floor(timeVar / 60 / 60);
+    let minutes = Math.floor(timeVar / 60) - hours * 60;
+    let seconds = timeVar % 60;
+
+    return [hours, minutes, seconds]
+      .map(v => (v < 10 ? '0' + v : v))
+      .filter((v, i) => v !== '00' || i > 0)
+      .join(':');
   };
 
-  const time = {
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  };
+  // Placeholder, will need to add logic for how much time to add to timer
+  let time = 86400;
 
   const [timeRemaining, setTimeRemaining] = useState(time);
+
+  // This will be used to decide how much time on timer
+  const daysOfWeek = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  /** Incrementally remove seconds, need to figure out why timeout has to
+   *   be set to 500 MS, should work at 1000
+   */
+  useEffect(() => {
+    const timer =
+      timeRemaining > 0 &&
+      setTimeout(() => setTimeRemaining(timeRemaining - 1), 500);
+    return () => timer;
+  }, [timeRemaining]);
+
+  return (
+    <div className="countdown-timer">
+      <div>Countdown: {convertTimeFormat(timeRemaining)}</div>
+    </div>
+  );
 };
