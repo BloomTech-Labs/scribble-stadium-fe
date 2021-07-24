74fb4bb7cae75f51f78491dfa948bedb6034b871
ngwatso <ngwatson79@yahoo.com>
2021-07-24 17:21:55 -0400

    countdown from current time fully functional, logic for end time to be completed

--- src/components/pages/HeadsUpDisplay/CountDownTimer.js
+++ src/components/pages/HeadsUpDisplay/CountDownTimer.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
 export const CountDownTimer = () => {
-  // This function will convert timer from seconds to HH:MM:SS format
+  // This function will calculate difference between current and target times
+  const calculateTimeRemaining = () => {
+    let currTime = new Date();
+    // Placeholder, need to add logic for endTime
+    let endTime = new Date('2021-07-25 03:00:00');
+
+    let timeDiff = endTime - currTime;
+
+    return timeDiff;
+  };
+
+  // This function will convert the format for our time difference to HH:MM:SS
   const convertTimeFormat = time => {
     let timeVar = parseInt(time, 10);
 
-    let hours = Math.floor(timeVar / 60 / 60);
-    let minutes = Math.floor(timeVar / 60) - hours * 60;
-    let seconds = timeVar % 60;
+    let hours = Math.floor(timeVar / (1000 * 60 * 60));
+    let minutes = Math.floor((timeVar / (1000 * 60)) % 60);
+    let seconds = Math.floor(timeVar / 1000) % 60;
 
     return [hours, minutes, seconds]
       .map(v => (v < 10 ? '0' + v : v))
@@ -15,31 +26,28 @@ export const CountDownTimer = () => {
       .join(':');
   };
 
-  // Placeholder, will need to add logic for how much time to add to timer
-  let time = 86400;
+  let time = calculateTimeRemaining();
 
   const [timeRemaining, setTimeRemaining] = useState(time);
 
-  // This will be used to decide how much time on timer
   const daysOfWeek = [
+    'Sunday',
     'Monday',
     'Tuesday',
     'Wednesday',
     'Thursday',
     'Friday',
     'Saturday',
-    'Sunday',
   ];
+  let today = new Date();
+  let day = daysOfWeek[today.getDay()];
+  // console.log(day);
 
-  /** Incrementally remove seconds, need to figure out why timeout has to
-   *   be set to 500 MS, should work at 1000
-   */
   useEffect(() => {
     const timer =
-      timeRemaining > 0 &&
-      setTimeout(() => setTimeRemaining(timeRemaining - 1), 500);
+      timeRemaining > 0 && setTimeout(() => setTimeRemaining(time), 1000);
     return () => timer;
-  }, [timeRemaining]);
+  }, [time, timeRemaining]);
 
   return (
     <div className="countdown-timer">
