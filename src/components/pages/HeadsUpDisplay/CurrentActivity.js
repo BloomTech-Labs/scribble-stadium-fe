04915bf763c48d4c5a7e34352c69efb245ff051d
ngwatso <ngwatson79@yahoo.com>
2021-08-05 16:14:44 -0400

    added state to current activity

--- src/components/pages/HeadsUpDisplay/CurrentActivity.js
+++ src/components/pages/HeadsUpDisplay/CurrentActivity.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export const CurrentActivity = () => {
+
+  const [day, setDay] = useState(1);
+  const [activity, setActivity] = useState("");
 };
