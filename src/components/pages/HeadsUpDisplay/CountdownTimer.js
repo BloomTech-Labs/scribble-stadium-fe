056b629fe3225cdc6480dee308505cd419116328
Nick Watson <67918013+ngwatso@users.noreply.github.com>
2021-08-04 22:48:04 -0400

    Feature/heads up countdown timer (#143)
    * set up environmental variables, app loading on localhost
    
    * added folder for HeadsUpDisplay components, created CountDownTimer component
    
    * created variable to calculate current time
    
    * reformatted countdown timer, functioning with static data input
    
    * countdown from current time fully functional, logic for end time to be completed
    
    * added logic for current activity
    
    * built logic to set endTime variable based on day of week
    
    * renamed component to CurrentActivity
    
    * changed 7 day format to 14 day format, need to add week-toggle logic
    
    * renamed feature to countdown timer, moved current activity logic to new component

---
+++ src/components/pages/HeadsUpDisplay/CountdownTimer.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+export const CountDownTimer = () => {
+  // This function will calculate difference between current and target times
+  const calculateTimeRemaining = () => {
+    let currTime = new Date();
+
+    // Set endTime to midnight each day, need to add logic for Saturday weekTwo to === 48 hours
+    let endTime = new Date().setHours(24, 0, 0, 0);
+
+    let timeDiff = endTime - currTime;
+
+    return timeDiff;
+  };
+
+  // This function will convert the format for our time difference to HH:MM:SS
+  const convertTimeFormat = time => {
+    let timeVar = parseInt(time, 10);
+
+    let hours = Math.floor(timeVar / (1000 * 60 * 60));
+    let minutes = Math.floor((timeVar / (1000 * 60)) % 60);
+    let seconds = Math.floor(timeVar / 1000) % 60;
+
+    return [hours, minutes, seconds]
+      .map(v => (v < 10 ? '0' + v : v))
+      .filter((v, i) => v !== '00' || i > 0)
+      .join(':');
+  };
+
+  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
+
+  useEffect(() => {
+    const timer =
+      timeRemaining > 0 &&
+      setTimeout(() => setTimeRemaining(calculateTimeRemaining()), 1000);
+    return () => clearTimeout(timer);
+  }, [timeRemaining]);
+
+  return (
+    <div className="countdown-timer">
+      <div>Time Remaining: {convertTimeFormat(timeRemaining)}</div>
+    </div>
+  );
+};
