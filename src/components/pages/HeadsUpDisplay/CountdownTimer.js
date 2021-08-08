b6e0177f3c7a2241d9f3d10aa9373798780ec0d4
ngwatso <ngwatson79@yahoo.com>
2021-08-08 11:53:35 -0400

    added NextActivity component to CountdownTimer to render

--- src/components/pages/HeadsUpDisplay/CountdownTimer.js
+++ src/components/pages/HeadsUpDisplay/CountdownTimer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { CurrentActivity } from './CurrentActivity'
+import { NextActivity } from './NextActivity'
 
 export const CountDownTimer = () => {
   // This function will calculate difference between current and target times
@@ -41,6 +42,7 @@ export const CountDownTimer = () => {
     <div className="countdown-timer">
       <div>Current Activity: <CurrentActivity /></div>
       <div>Time Remaining: {convertTimeFormat(timeRemaining)}</div>
+      <div>Next Activity: <NextActivity /></div>
     </div>
   );
 };
