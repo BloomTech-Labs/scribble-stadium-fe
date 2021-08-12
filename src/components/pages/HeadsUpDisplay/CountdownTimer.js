c4ff194fdedd9822e03f90d00fc2178a1f13a180
ngwatso <ngwatson79@yahoo.com>
2021-08-11 23:20:05 -0400

    added conversion to pacific standard time in countdown timer

--- src/components/pages/HeadsUpDisplay/CountdownTimer.js
+++ src/components/pages/HeadsUpDisplay/CountdownTimer.js
@@ -1,21 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { CurrentActivity } from './CurrentActivity'
+import React, { useState, useEffect } from "react";
+import { CurrentActivity } from "./activity";
+import { NextActivity } from "./currentActivity";
 
 export const CountDownTimer = () => {
   // This function will calculate difference between current and target times
   const calculateTimeRemaining = () => {
     let currTime = new Date();
 
+    // This variable will format current local time to pacific timezone (string)
+    let pst = currTime.toLocaleString("en-US", {
+      timeZone: "America/Los_Angeles",
+      weekday: "short",
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric"
+    });
+
     // Set endTime to midnight each day, need to add logic for Saturday weekTwo to === 48 hours
     let endTime = new Date().setHours(24, 0, 0, 0);
-
-    let timeDiff = endTime - currTime;
+    // parse var pst, changing it from string to number format, then subtract from endTime
+    let timeDiff = endTime - Date.parse(pst);
 
     return timeDiff;
   };
 
   // This function will convert the format for our time difference to HH:MM:SS
-  const convertTimeFormat = time => {
+  const convertTimeFormat = (time) => {
     let timeVar = parseInt(time, 10);
 
     let hours = Math.floor(timeVar / (1000 * 60 * 60));
@@ -23,9 +36,9 @@ export const CountDownTimer = () => {
     let seconds = Math.floor(timeVar / 1000) % 60;
 
     return [hours, minutes, seconds]
-      .map(v => (v < 10 ? '0' + v : v))
-      .filter((v, i) => v !== '00' || i > 0)
-      .join(':');
+      .map((v) => (v < 10 ? "0" + v : v))
+      .filter((v, i) => v !== "00" || i > 0)
+      .join(":");
   };
 
   const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
@@ -39,8 +52,9 @@ export const CountDownTimer = () => {
 
   return (
     <div className="countdown-timer">
-      <div>Current Activity: <CurrentActivity /></div>
-      <div>Time Remaining: {convertTimeFormat(timeRemaining)}</div>
+      <CurrentActivity />
+      <div>{convertTimeFormat(timeRemaining)}</div>
+      <NextActivity />
     </div>
   );
 };
