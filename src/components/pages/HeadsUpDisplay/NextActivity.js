f820b2fe68a24c3095483c50a476fa9e26b58b43
Nick Watson <67918013+ngwatso@users.noreply.github.com>
2021-08-13 01:33:31 -0400

    Refactored functions to determine current day (#147)
    * added NextActivity component to CountdownTimer to render
    
    * refactored function to determine current day to improve readability
    
    * refactored nextday component to be more readable
    
    * fixed rotation, had set it to 14 day cycle, set it back to 7 day cycle
    
    * Fixed activity name on day 6, 'Big Final Reveal' to 'Voting'

--- src/components/pages/HeadsUpDisplay/NextActivity.js
+++ src/components/pages/HeadsUpDisplay/NextActivity.js
@@ -1,76 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 export const NextActivity = () => {
-
   const [day, setDay] = useState(1);
   const [activity, setActivity] = useState("");
 
-  // This function will select next day in 14 day rotation
-  // Will reconfigure to make code more dry later
-  const nextDay = (day) => {
+  // Set current day in state
+  const nextDay = () => {
     let today = new Date().getDay();
+
     setDay(today);
-    if (today === 0) {
-      if (day === 13) {
-        setDay(0);
-        return;
-        // return day;
-      } else if (day === 6) {
-        setDay(7);
-        return;
-      }
-    } else if (today === 1) {
-      if (day === 0) {
-        setDay(1);
-        return;
-      } else if (day === 7) {
-        setDay(8);
-        return;
-      }
-    } else if (today === 2) {
-      if (day === 1) {
-        setDay(2);
-        return;
-      } else if (day === 8) {
-        setDay(9);
-        return;
-      }
-    } else if (today === 3) {
-      if (day === 2) {
-        setDay(3);
-        return;
-      } else if (day === 9) {
-        setDay(10);
-        return;
-      }
-    } else if (today === 4) {
-      if (day === 3) {
-        setDay(4);
-        return;
-      } else if (day === 10) {
-        setDay(11);
-        return;
-      }
-    } else if (today === 5) {
-      if (day === 4) {
-        setDay(5);
-        return;
-      } else if (day === 11) {
-        setDay(13);
-        return;
-      }
-    } else if (today === 6) {
-      if (day === 5) {
-        setDay(6);
-        return;
-      } else if (day === 12) {
-        setDay(13);
-        return;
-      }
-    }
   };
 
-  // This function will find the next activity based on day
+  // This function will find the next activity based on next day and set to state
   const nextActivity = () => {
     const activityList = {
       0: "Big Final Reveal",
@@ -79,23 +20,17 @@ export const NextActivity = () => {
       3: "Write",
       4: "Squadding Up",
       5: "Point Share",
-      6: "Matchup 1",
-      7: "Voting Session 1",
-      8: "Matchup 2",
-      9: "Voting Session 2",
-      10: "Matchup 3",
-      11: "Voting Session 3",
-      12: "Matchup 4",
-      13: "Big Final Reveal"
+      6: "Voting"
     };
-    setActivity(activityList[day+1]);
+    setActivity(day === 6 ? activityList[0] : activityList[day + 1]);
     return activity;
   };
 
+  // useEffect to call both functions on render
   useEffect(() => {
     nextDay();
     nextActivity();
   });
 
   return <div>Next Activity: {activity}</div>;
-}
+};
