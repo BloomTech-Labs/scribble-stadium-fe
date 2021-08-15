c126ad738e1aac5e6a2026729bf2e98b72df56b4
ngwatso <ngwatson79@yahoo.com>
2021-08-15 00:25:20 -0400

    refactored function to determine current day to improve readability

--- src/components/pages/HeadsUpDisplay/CurrentActivity.js
+++ src/components/pages/HeadsUpDisplay/CurrentActivity.js
@@ -1,72 +1,35 @@
 import React, { useState, useEffect } from "react";
 
 export const CurrentActivity = () => {
-
   const [day, setDay] = useState(1);
   const [activity, setActivity] = useState("");
 
-  // This function will change current day (0-13) based on previous day
-  // Will come back and refactor this to make it dry later
-  const currDay = (day) => {
+  const currDay = () => {
     let today = new Date().getDay();
-    setDay(today);
-    if (today === 0) {
-      if (day === 13) {
-        setDay(0);
+
+    const checkDay = [
+      [13, 6],
+      [0, 7],
+      [1, 8],
+      [2, 9],
+      [3, 10],
+      [4, 11],
+      [5, 12]
+    ];
+
+    let prevDay = checkDay[today];
+
+    // This funcion will set current day based on previous day
+    const calcDay = () => {
+      if (day === prevDay[1]) {
+        setDay(prevDay[1] + 1);
         return;
-      } else if (day === 6) {
-        setDay(7);
-        return;
-      }
-    } else if (today === 1) {
-      if (day === 0) {
-        setDay(1);
-        return;
-      } else if (day === 7) {
-        setDay(8);
-        return;
-      }
-    } else if (today === 2) {
-      if (day === 1) {
-        setDay(2);
-        return;
-      } else if (day === 8) {
-        setDay(9);
+      } else if (day === prevDay[0]) {
+        prevDay[0] === 13 ? setDay(0) : setDay(prevDay[0] + 1);
         return;
       }
-    } else if (today === 3) {
-      if (day === 2) {
-        setDay(3);
-        return;
-      } else if (day === 9) {
-        setDay(10);
-        return;
-      }
-    } else if (today === 4) {
-      if (day === 3) {
-        setDay(4);
-        return;
-      } else if (day === 10) {
-        setDay(11);
-        return;
-      }
-    } else if (today === 5) {
-      if (day === 4) {
-        setDay(5);
-        return;
-      } else if (day === 11) {
-        setDay(12);
-        return;
-      }
-    } else if (today === 6) {
-      if (day === 5) {
-        setDay(6);
-        return;
-      } else if (day === 12) {
-        setDay(13);
-        return;
-      }
-    }
+    };
+    calcDay();
   };
 
   // This function will find the current activity based on current day and set to state
