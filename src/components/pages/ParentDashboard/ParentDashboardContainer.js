09ecda4a3cb7c773b1241d4d31f385d9b0fc4c04
da-vazquez <da.vazquez@yahoo.com>
2021-08-23 18:13:48 -0700

    replaced okta authenication to auth0 isAuthenticated built-in hook. these files were changed due to rendering errors only

--- src/components/pages/ParentDashboard/ParentDashboardContainer.js
+++ src/components/pages/ParentDashboard/ParentDashboardContainer.js
@@ -1,24 +1,25 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { useOktaAuth } from '@okta/okta-react';
-
+import { useAuth0 } from '@auth0/auth0-react'; //replaces useOtkaAuth
 import RenderParentDashboard from './RenderParentDashboard';
 import { connect } from 'react-redux';
 
 function ParentDashboardContainer({ LoadingComponent, ...props }) {
-  const { authState, authService } = useOktaAuth();
+  const { user, isAuthenticated } = useAuth0();
   const [userInfo, setUserInfo] = useState(null);
   // eslint-disable-next-line
-  const [memoAuthService] = useMemo(() => [authService], []);
+  const [memoAuthService] = useMemo(() => [user], []);
 
-  useEffect(() => {
-    let isSubscribed = true;
+  /* do we need code below since auth0 has built-in isAuthenticated and user info hook? */
+
+  /* useEffect(() => {
+    let isAuthenticated = true;
 
     memoAuthService
       .getUser()
       .then(info => {
         // if user is authenticated we can use the authService to snag some user info.
         // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
-        if (isSubscribed) {
+        if (isAuthenticated) {
           setUserInfo(info);
         }
       })
@@ -26,20 +27,16 @@ function ParentDashboardContainer({ LoadingComponent, ...props }) {
         // isSubscribed = false;
         setUserInfo(null);
       });
-    return () => (isSubscribed = false);
-  }, [memoAuthService]);
+    return () => (isAuthenticated = false);
+  }, [memoAuthService]); */
 
   return (
     <>
-      {authState.isAuthenticated && !userInfo && (
+      {isAuthenticated && !userInfo && (
         <LoadingComponent message="Loading..." />
       )}
-      {authState.isAuthenticated && userInfo && (
-        <RenderParentDashboard
-          {...props}
-          userInfo={userInfo}
-          authService={authService}
-        />
+      {isAuthenticated && userInfo && (
+        <RenderParentDashboard {...props} userInfo={userInfo} user={user} />
       )}
     </>
   );
