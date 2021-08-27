224efec29d307f872cc2013b131a7f0d2009ab80
da-vazquez <da.vazquez@yahoo.com>
2021-08-26 23:20:47 -0700

    updates to fix faling tests, related to userInfo prop

--- src/components/pages/NewParentDashboard/NewParentDashboardContainer.js
+++ src/components/pages/NewParentDashboard/NewParentDashboardContainer.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import RenderNewParentDashboard from './RenderNewParentDashboard';
 import { connect } from 'react-redux';
 
 function NewParentDashboardContainer({ LoadingComponent, ...props }) {
   const { user, isAuthenticated } = useAuth0();
+  const [userInfo, setUserInfo] = useState(user);
 
   return (
     <>
-      {isAuthenticated && !user && <LoadingComponent message="Loading..." />}
-      {isAuthenticated && user && (
-        <RenderNewParentDashboard {...props} user={user} />
+      {isAuthenticated && !userInfo && (
+        <LoadingComponent message="Loading..." />
+      )}
+      {isAuthenticated && userInfo && (
+        <RenderNewParentDashboard {...props} userInfo={userInfo} />
       )}
     </>
   );
--- src/components/pages/ParentDashboard/ParentDashboardContainer.js
+++ src/components/pages/ParentDashboard/ParentDashboardContainer.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import RenderParentDashboard from './RenderParentDashboard';
 import { connect } from 'react-redux';
 
 function ParentDashboardContainer({ LoadingComponent, ...props }) {
   const { user, isAuthenticated } = useAuth0();
+  const [userInfo, setUserInfo] = useState(user);
 
   return (
     <>
-      {isAuthenticated && !user && <LoadingComponent message="Loading..." />}
-      {isAuthenticated && user && (
-        <RenderParentDashboard {...props} user={user} />
+      {isAuthenticated && !userInfo && (
+        <LoadingComponent message="Loading..." />
+      )}
+      {isAuthenticated && userInfo && (
+        <RenderParentDashboard {...props} userInfo={userInfo} />
       )}
     </>
   );
