3b28c12351c38beb463e66cef96c167883585272
da-vazquez <da.vazquez@yahoo.com>
2021-08-26 19:25:28 -0700

    useMemo & useEffect commented functions removed

--- src/components/pages/NewParentDashboard/NewParentDashboardContainer.js
+++ src/components/pages/NewParentDashboard/NewParentDashboardContainer.js
@@ -1,42 +1,16 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import RenderNewParentDashboard from './RenderNewParentDashboard';
 import { connect } from 'react-redux';
 
 function NewParentDashboardContainer({ LoadingComponent, ...props }) {
   const { user, isAuthenticated } = useAuth0();
-  const [userInfo, setUserInfo] = useState(user);
-  // eslint-disable-next-line
-  const [memoAuthService] = useMemo(() => [user], []);
-
-  /* do we need code below since auth0 has built-in isAuthenticated hook? */
-
-  /* useEffect(() => {
-    let isAuthenticated = true;
-
-    memoAuthService
-      .getUser()
-      .then(info => {
-        // if user is authenticated we can use the authService to snag some user info.
-        // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
-        if (isAuthenticated) {
-          setUserInfo(info);
-        }
-      })
-      .catch(err => {
-        // isSubscribed = false;
-        setUserInfo(null);
-      });
-    return () => (isAuthenticated = false);
-  }, [memoAuthService]); */
 
   return (
     <>
-      {isAuthenticated && !userInfo && (
-        <LoadingComponent message="Loading..." />
-      )}
-      {isAuthenticated && userInfo && (
-        <RenderNewParentDashboard {...props} userInfo={userInfo} user={user} />
+      {isAuthenticated && !user && <LoadingComponent message="Loading..." />}
+      {isAuthenticated && user && (
+        <RenderNewParentDashboard {...props} user={user} />
       )}
     </>
   );
--- src/components/pages/ParentDashboard/ParentDashboardContainer.js
+++ src/components/pages/ParentDashboard/ParentDashboardContainer.js
@@ -1,42 +1,16 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import RenderParentDashboard from './RenderParentDashboard';
 import { connect } from 'react-redux';
 
 function ParentDashboardContainer({ LoadingComponent, ...props }) {
   const { user, isAuthenticated } = useAuth0();
-  const [userInfo, setUserInfo] = useState(null);
-  // eslint-disable-next-line
-  const [memoAuthService] = useMemo(() => [user], []);
-
-  /* do we need code below since auth0 has built-in isAuthenticated and user info hook? */
-
-  /* useEffect(() => {
-    let isAuthenticated = true;
-
-    memoAuthService
-      .getUser()
-      .then(info => {
-        // if user is authenticated we can use the authService to snag some user info.
-        // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
-        if (isAuthenticated) {
-          setUserInfo(info);
-        }
-      })
-      .catch(err => {
-        // isSubscribed = false;
-        setUserInfo(null);
-      });
-    return () => (isAuthenticated = false);
-  }, [memoAuthService]); */
 
   return (
     <>
-      {isAuthenticated && !userInfo && (
-        <LoadingComponent message="Loading..." />
-      )}
-      {isAuthenticated && userInfo && (
-        <RenderParentDashboard {...props} userInfo={userInfo} user={user} />
+      {isAuthenticated && !user && <LoadingComponent message="Loading..." />}
+      {isAuthenticated && user && (
+        <RenderParentDashboard {...props} user={user} />
       )}
     </>
   );
