c69c7dec34e23d63f3cc1c47fba2053f10ba911a
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2021-08-28 16:54:30 -0500

    Updated ProfileModalContainer.js with useAuth0.

--- src/components/pages/Modal/ProfileModalContainer.js
+++ src/components/pages/Modal/ProfileModalContainer.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import { useOktaAuth } from '@okta/okta-react';
+import React, { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 import { connect } from 'react-redux';
 import { child, parent } from '../../../state/actions';
@@ -7,35 +7,15 @@ import { child, parent } from '../../../state/actions';
 import ProfileRenderModal from './ProfileRenderModal';
 
 function ProfileModalContainer({ LoadingComponent, ...props }) {
-  const { authState, authService } = useOktaAuth();
-  const [userInfo, setUserInfo] = useState(null);
-  // eslint-disable-next-line
-  const [memoAuthService] = useMemo(() => [authService], []);
+  const { user, isAuthenticated } = useAuth0();
+  const [userInfo, setUserInfo] = useState(user);
 
-  useEffect(() => {
-    let isSubscribed = true;
-
-    memoAuthService
-      .getUser()
-      .then(info => {
-        // if user is authenticated we can use the authService to snag some user info.
-        // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
-        if (isSubscribed) {
-          setUserInfo(info);
-        }
-      })
-      .catch(err => {
-        isSubscribed = false;
-        return setUserInfo(null);
-      });
-    return () => (isSubscribed = false);
-  }, [memoAuthService]);
   return (
     <>
-      {authState.isAuthenticated && !userInfo && (
+      {isAuthenticated && !userInfo && (
         <LoadingComponent message="Loading..." />
       )}
-      {authState.isAuthenticated && userInfo && (
+      {isAuthenticated && userInfo && (
         <ProfileRenderModal
           {...props}
           userInfo={userInfo}
