b6bf3a87e2f733d3213e18996c6af892c04220a6
da-vazquez <da.vazquez@yahoo.com>
2021-08-28 20:10:41 -0700

    Removed useEffect and useMemo, no longer used

--- src/components/pages/ChildDashboard/ChildDashboardContainer.js
+++ src/components/pages/ChildDashboard/ChildDashboardContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 import { connect } from 'react-redux';
