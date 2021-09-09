8bca15e79b21eca69b4cc5a3a2fda8712378e09d
da-vazquez <da.vazquez@yahoo.com>
2021-09-09 00:12:57 -0700

    deleted file due to switch from okta to Auth0

--- src/utils/oktaConfig.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  issuer: process.env.REACT_APP_OKTA_ISSUER_URI,
-  redirectUri: window.location.origin + '/implicit/callback',
-  clientId: process.env.REACT_APP_CLIENT_ID,
-  pkce: true,
-  scopes: ['openid', 'email', 'profile'],
-};
-
-export { config };
