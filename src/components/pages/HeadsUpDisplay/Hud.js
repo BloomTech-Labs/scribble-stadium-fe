feb608bce453a2997286a5420b6e62f5e07a5bc2
BrittanyCanty <bcanty92@gmail.com>
2021-09-08 21:12:29 -0500

    Inserted Divs inside button

--- src/components/pages/HeadsUpDisplay/Hud.js
+++ src/components/pages/HeadsUpDisplay/Hud.js
@@ -3,6 +3,8 @@ import { CountDownTimer } from './CountdownTimer';
 import Hud from '../../../styles/less/Hud.less';
 import { Button } from 'antd';
 import { UpCircleFilled } from '@ant-design/icons';
+import { DownCircleFilled } from '@ant-design/icons';
+import { Collapse } from 'antd';
 
 export default function (props) {
   const { completedActivity, currentActivity, currentBar } = props;
@@ -16,46 +18,58 @@ export default function (props) {
   ];
 
   const dayBars = ['bar1', 'bar2', 'bar3', 'bar4'];
+  const { Panel } = Collapse;
 
   return (
     <div className="HudContainer">
-      <CountDownTimer />
-      <br />
-      {/* This is going to show all tasks as well as indicating current task */}
-      <div className="progressionBar">
-        {activities.map(a => {
-          return (
-            <div
-              className={`activity ${currentActivity == a && 'currentActivity'}
+      <Collapse
+        bordered={false}
+        defaultActiveKey={['1']}
+        expandIcon={({ isActive }) => (
+          <DownCircleFilled rotate={isActive ? 90 : 0} />
+        )}
+        className="site-collapse-custom-collapse"
+      >
+        <Panel header="" key="1" className="site-collapse-custom-panel">
+          <CountDownTimer />
+          <br />
+          {/* This is going to show all tasks as well as indicating current task */}
+          <div className="progressionBar">
+            {activities.map(a => {
+              return (
+                <div
+                  className={`activity ${
+                    currentActivity == a && 'currentActivity'
+                  }
               ${
                 completedActivity.length - 1 >= activities.indexOf(a) &&
                 'completedActivity'
               }`}
-            >
-              <span>{a}</span>
-            </div>
-          );
-        })}
-      </div>
-      {/* This will show the days and indicaate current day */}
+                >
+                  <span>{a}</span>
+                </div>
+              );
+            })}
+          </div>
+          <div className="dayBars">
+            {dayBars.map(a => {
+              return (
+                <div
+                  className={`${a} ${currentBar == a && 'currentBar'}`}
+                ></div>
+              );
+            })}
+          </div>
 
-      <div className="dayBars">
-        {dayBars.map(a => {
-          return (
-            <div className={`${a} ${currentBar == a && 'currentBar'}`}></div>
-          );
-        })}
-      </div>
-
-      <div className="days">
-        <div className="day1-3">Day 1 - 3</div>
-        <div className="day4">Day 4</div>
-        <div className="day5">Day 5</div>
-        <div className="day6-7">Day 6 - 7</div>
-      </div>
-      {/* Button for collapsing/expanding will go here */}
-      <input type="checkbox" name="toggle" id="toggle" />
-      <label for="toggle"></label>
+          <div className="days">
+            <div className="day1-3">Day 1 - 3</div>
+            <div className="day4">Day 4</div>
+            <div className="day5">Day 5</div>
+            <div className="day6-7">Day 6 - 7</div>
+          </div>
+        </Panel>
+      </Collapse>
+      ,
     </div>
   );
 }
