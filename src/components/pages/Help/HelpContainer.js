68283e0368907659f5a80cd1950fba5c0b4dcf3e
SethMacP <macpherson.seth@gmail.com>
2021-09-08 18:03:55 -0700

    useState hook set to user instead of null

--- src/components/pages/Help/HelpContainer.js
+++ src/components/pages/Help/HelpContainer.js
@@ -5,7 +5,7 @@ import Help from './Help';
 
 function HelpContainer({ LoadingComponent }) {
   const { user, isAuthenticated } = useAuth0();
-  const [userInfo, setUserInfo] = useState(null);
+  const [userInfo, setUserInfo] = useState(user);
 
   return (
     <>
