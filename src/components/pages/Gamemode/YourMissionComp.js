86ef53c39166c83ed55507066090ff1582435dfe
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-09 08:44:11 -0500

    removed rwd.draw assignment #22

--- src/components/pages/Gamemode/YourMissionComp.js
+++ src/components/pages/Gamemode/YourMissionComp.js
@@ -19,7 +19,6 @@ const YourMissionComp = ({ ...props }) => {
       props.child.gamemode.write = true;
 
       props.child.gamemode.draw = false;
-      rwd.draw = false;
 
       props.child.gamemode.read = true;
       rwd.read = true;
