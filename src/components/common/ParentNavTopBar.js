ca597bee6a4b99c4074e4adfe737919064c3576c
Lakendria <kendra939@gmail.com>
2021-10-09 23:21:56 -0400

    added href attribute

--- src/components/common/ParentNavTopBar.js
+++ src/components/common/ParentNavTopBar.js
@@ -64,6 +64,7 @@ const ParentNavTopBar = props => {
           placement="bottomCenter"
         >
           <a
+            href="http://localhost:3000/parent/dashboard#"
             className="parent-avatar"
             data-testid="parent-avatar"
             onClick={e => e.preventDefault()}
