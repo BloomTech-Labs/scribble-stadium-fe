4e303e250304b71031f9939886f8c8b45aea0b53
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-14 05:55:29 -0500

    Removed Button import #3

--- src/components/pages/Gamemode/Thrashbar.jsx
+++ src/components/pages/Gamemode/Thrashbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Row, Col, Button } from 'antd';
+import { Row, Col } from 'antd';
 import { connect } from 'react-redux';
 
 class Thrashbar extends React.Component {
