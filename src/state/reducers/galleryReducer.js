03661e40cbd6e285d9a8ac98d2cb45cbe11334e4
artofmayhem <chef_tony_miller@yahoo.com>
2021-10-14 14:13:22 -1000

    fixed errant code in gallery reducer

--- src/state/reducers/galleryReducer.js
+++ src/state/reducers/galleryReducer.js
@@ -1,7 +1,6 @@
 import { submissions } from '../actions';
 
 const initialState = {
-feature/update-gallery-reducer-schema
   WritingUrl: '',
   PageNum: 0,
   DrawingUrl: '',
@@ -19,8 +18,6 @@ feature/update-gallery-reducer-schema
   LowConfidence: false,
   CreatedAt: '',
   UpdatedAt: '',
-
-
 };
 
 export const reducer = (state = initialState, action) => {
@@ -28,7 +25,7 @@ export const reducer = (state = initialState, action) => {
     case submissions.SET_WEEKLY_SUBMISSIONS:
       return {
         ...state,
-       feature/update-gallery-reducer-schema
+
         WritingUrl: action.payload.WritingUrl,
         PageNum: action.payload.PageNum,
         DrawingUrl: action.payload.DrawingUrl,
