00f98c38c2fdd71a91576103536242d6db817c9e
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-10-20 23:38:56 -0700

    Modified the payload object to match what is being received from the endpoint

--- src/state/actions/newStories.js
+++ src/state/actions/newStories.js
@@ -2,9 +2,7 @@ import { getStory } from '../../api/index';
 
 export const GET_NEW_STORIES = 'GET_NEW_STORIES';
 export const getNewStories = storyId => dispatch => {
-  getStory(storyId).then(res =>
-    dispatch({ type: GET_NEW_STORIES, payload: res.data }).catch(err =>
-      console.log(err)
-    )
-  );
+  getStory(storyId).then(res => {
+    dispatch({ type: GET_NEW_STORIES, payload: res });
+  });
 };
