9c782cb548fec45318e9055258ffb549cdba0c20
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-10-27 11:47:53 -1000

    Repairing the Gameplay logic for Happy Path  (#262)
    * re-imported all components into index.js
    
    * re-installed all previous routes
    
    * Re-inserting WritingSub
    
    * Re-inserting DrawingSub
    
    * re-name RenderingWritingSub file name => mis-named
    
    * reset Gamemode.js
    
    * reset GamemodeStyles.less
    
    * Adding Play Again Component to index.js

---
+++ src/components/pages/DrawingSub/DrawingSubContainer.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+import RenderDrawingSub from './RenderDrawingSub';
+import { connect } from 'react-redux';
+
+function DrawingSubContainer({ LoadingComponent, ...props }) {
+  const { user, isAuthenticated } = useAuth0();
+  const [userInfo] = useState(user);
+
+  return (
+    <>
+      {isAuthenticated && !userInfo && (
+        <LoadingComponent message="Loading..." />
+      )}
+      {isAuthenticated && userInfo && (
+        <RenderDrawingSub {...props} userInfo={userInfo} />
+      )}
+    </>
+  );
+}
+
+export default connect(
+  state => ({
+    parent: state.parent,
+  }),
+  {}
+)(DrawingSubContainer);
