05cafe4963a70049e867bf260554f15bcf7a4afb
alexandre steinhauslin <a.steinhauslin@gmail.com>
2021-10-27 15:56:53 -0700

    modified styling of settings to make it more similar to childform

--- src/components/pages/FamilySettings/RenderFamilySettings.js
+++ src/components/pages/FamilySettings/RenderFamilySettings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { Layout, Form, Card, Input, Button, Typography, Row, Col } from 'antd';
+import { Layout, Form, Card, Row, Input, Button, Typography } from 'antd';
 
 import ParentNavTopBar from '../../common/ParentNavTopBar';
 import ChildCard from '../../common/ChildCard';
@@ -32,127 +32,115 @@ const FamilySettings = props => {
   return (
     <Layout className="edit-players">
       <ParentNavTopBar selected="settings" />
+      <Layout className="content">
+        <div className="top-section">
+          <ParentDashboardBack />
+          <Title className="title" level={2}>
+            Settings
+          </Title>
+        </div>
+        <Layout className="children" style={{ flexFlow: 'row wrap' }}>
+          {props.parent.children.map(child => (
+            <ChildCard
+              name={child.Name}
+              AvatarURL={child.AvatarURL}
+              update="SETTINGS"
+            />
+          ))}
+        </Layout>
 
-      <div className="top-section">
-        <ParentDashboardBack />
-        <Title className="title" level={2}>
-          Settings
-        </Title>
-      </div>
-      <Layout className="children" style={{ flexFlow: 'row wrap' }}>
-        {props.parent.children.map(child => (
-          <ChildCard
-            name={child.Name}
-            AvatarURL={child.AvatarURL}
-            update="SETTINGS"
-          />
-        ))}
+        <Layout className="children">
+          <Card className="child-card-form" bordered={false}>
+            <div className="card-center">
+              <h3>Change Email or Password</h3>
+              <Form
+                {...layoutSettings}
+                form={form}
+                name="change-family-settings"
+                onFinish={onFinish}
+              >
+                <div className="col">
+                  <Form.Item
+                    label="Old Email"
+                    name="email"
+                    onChange={handleChange}
+                    rules={[
+                      {
+                        type: 'email',
+                        message: 'The input is not valid E-mail!',
+                      },
+                      {
+                        required: true,
+                        message: 'Please enter an email address!',
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                </div>
+                <div className="col">
+                  <Form.Item
+                    name="password"
+                    label="Password"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Please input your old password!',
+                      },
+                    ]}
+                    hasFeedback
+                  >
+                    <Input type="password" />
+                  </Form.Item>
+                </div>
+                <div className="col">
+                  <Form.Item
+                    name="email"
+                    label="New Email"
+                    onChange={handleChange}
+                    rules={[
+                      {
+                        type: 'email',
+                        message: 'The input is not valid E-mail!',
+                      },
+                      {
+                        required: true,
+                        message: 'Please enter an email address!',
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                </div>
+                <div className="col">
+                  <Form.Item
+                    label="Re-Enter New Email"
+                    name="email"
+                    onChange={handleChange}
+                    rules={[
+                      {
+                        type: 'email',
+                        message: 'The input is not valid E-mail!',
+                      },
+                      {
+                        required: true,
+                        message: 'Please enter an email address!',
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                </div>
+                <div className="card-bottom">
+                  <Button className="save-btn" htmlType="submit" disabled>
+                    Update
+                  </Button>
+                </div>
+              </Form>
+            </div>
+          </Card>
+        </Layout>
       </Layout>
-
-      <Row className="card">
-        <Col>
-          <Row className="title">
-            <h4>Change Email or Password</h4>
-          </Row>
-          <Form
-            {...layoutSettings}
-            form={form}
-            name="change-family-settings"
-            onFinish={onFinish}
-          >
-            <Row className="form-inputs">
-              <Col>
-                <label htmlFor="oldEmail">Enter Old Email</label>
-                <br />
-                <input
-                  id="oldEmail"
-                  name="email"
-                  onChange={handleChange}
-                  rules={[
-                    {
-                      type: 'email',
-                      message: 'The input is not valid E-mail!',
-                    },
-                    {
-                      required: true,
-                      message: 'Please enter an email address!',
-                    },
-                  ]}
-                  className="contact-form-input"
-                />
-              </Col>
-              <Col>
-                <label htmlFor="oldPassword">Enter Old Password</label>
-                <br />
-                <input
-                  id="oldPassword"
-                  name="password"
-                  rules={[
-                    {
-                      required: true,
-                      type: 'password',
-                      message: 'Please input your old password!',
-                    },
-                  ]}
-                  hasFeedback
-                  className="contact-form-input"
-                />
-              </Col>
-            </Row>
-            <Row className="form-inputs">
-              <Col>
-                <label htmlFor="newEmail">Enter New Email</label>
-                <br />
-                <input
-                  id="newEmail"
-                  name="email"
-                  onChange={handleChange}
-                  rules={[
-                    {
-                      type: 'email',
-                      message: 'The input is not valid E-mail!',
-                    },
-                    {
-                      required: true,
-                      message: 'Please enter an email address!',
-                    },
-                  ]}
-                  className="contact-form-input"
-                />
-              </Col>
-              <Col>
-                <label htmlFor="ReEnterNewEmail">Re-Enter New Email</label>
-                <br />
-                <input
-                  id="ReEnterNewEmail"
-                  name="email"
-                  onChange={handleChange}
-                  rules={[
-                    {
-                      type: 'email',
-                      message: 'The input is not valid E-mail!',
-                    },
-                    {
-                      required: true,
-                      message: 'Please enter an email address!',
-                    },
-                  ]}
-                  className="contact-form-input"
-                />
-              </Col>
-            </Row>
-            <Row>
-              <input
-                type="submit"
-                size="large"
-                value="UPDATE"
-                htmlType="submit"
-                className="contact-form-submit-btn"
-              ></input>
-            </Row>
-          </Form>
-        </Col>
-      </Row>
     </Layout>
   );
 };
