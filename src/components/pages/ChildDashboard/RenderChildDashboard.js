a62b5553b16006c8557ad216d15811301e16d0b5
artofmayhem <chef_tony_miller@yahoo.com>
2021-11-02 13:47:17 -1000

    resolved PR change request

--- src/components/pages/ChildDashboard/RenderChildDashboard.js
+++ src/components/pages/ChildDashboard/RenderChildDashboard.js
@@ -6,7 +6,6 @@ import { InstructionsModal } from '../../common';
 import { modalInstructions } from '../../../utils/helpers';
 import { HUD } from '../HeadsUpDisplay/index';
 import Explosion from '../../../assets/images/gamemodeimg/explosion.png';
-// import adventure_passport from '../../../assets/images/child_dashboard_images/adventure_passport.svg';
 import change_your_avatar from '../../../assets/images/child_dashboard_images/change_your_avatar.svg';
 import leaderboard_icon from '../../../assets/images/child_dashboard_images/leaderboard_icon.png';
 
