10d985091f958d5dd38f203095b8ffc053e15439
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-11-03 11:24:13 -0700

    WIP styling Point Share Page

--- src/components/pages/PointShare/RenderPointShare.js
+++ src/components/pages/PointShare/RenderPointShare.js
@@ -3,13 +3,24 @@ import { useHistory } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Header } from '../../common';
 import { Row, Col, InputNumber, Button, notification } from 'antd';
-import { QuestionCircleOutlined } from '@ant-design/icons';
+import {
+  ZoomInOutlined,
+  CaretUpOutlined,
+  CaretDownOutlined,
+} from '@ant-design/icons';
 import { connect } from 'react-redux';
 import { submitPoints } from '../../../api/index';
 
 import { SubmissionViewerModal } from '../../common';
 import { InstructionsModal } from '../../common';
 import { modalInstructions } from '../../../utils/helpers';
+
+// import hero images - TEMPORARY HARD CODE
+import childOneImg from '../../../assets/images/hero_images/hero10.png';
+import childTwoImg from '../../../assets/images/hero_images/hero8.png';
+// import sample writing submission - TEMPORARY HARD CODE
+import sampleWritingSubmission from './sampleWriting.png';
+import sampleDrawingSubmission from './sampleDrawing.png';
 
 const PointShare = props => {
   const { push } = useHistory();
@@ -158,161 +169,144 @@ const PointShare = props => {
         />
       )}
       <Header
-        title="SHARE POINTS"
+        title="SCRIBBLE STADIUM"
         displayMenu={true}
         pointsRemaining={true}
         points={totalPoints}
         teamName={true}
       />
-      <QuestionCircleOutlined
-        className="question-icon"
-        onClick={() => {
-          setModalVisible(true);
-        }}
-      />
-      <InstructionsModal
-        modalVisible={modalVisible}
-        handleCancel={() => {
-          setModalVisible(false);
-        }}
-        handleOk={() => {
-          setModalVisible(false);
-        }}
-        instructions={modalInstructions.sharePoints}
-      />
-      <div className="point-share-container">
-        <Row className="team-row">
-          <Col>
-            <Row className="teammate-one">
-              <img
-                className="teammate-one-avatar"
-                src={props.team.child1.AvatarURL}
-                alt="Child Avatar"
+
+      <div className="point-share-rectangle">
+        <div className="point-share-text">
+          <h2 className="point-share-text-heading">POINTS SHARING</h2>
+          <p className="point-share-paragraph">{`Now it is time to split up 100 points across your squad's portfolio of stories and drawings.  You must put a minimum of 10 points on each and the total among all 4 must add up to 100 points.`}</p>
+
+          <p className="point-share-paragraph">
+            {`As you read both stories and look at both drawings, think about which ones best reflect the characters,  plot, and settings from the chapters you read.  Share the most points with the ones you think are the best.`}{' '}
+          </p>
+        </div>
+      </div>
+
+      <div className="point-share-boxes-container">
+        <div className="point-share-box child-one">
+          <div className="point-share-child-info">
+            <img src={childOneImg} alt="child-one" />
+            <p className="point-share-heading">{`PINKY WINKY (YOU)`}</p>
+          </div>
+          <div className="point-share-story">
+            <p className="point-share-heading">STORY</p>
+            <div className="point-share-submission">
+              <img src={sampleWritingSubmission} alt="child-one-story" />
+            </div>
+            <div className="point-share-zoom-container">
+              <ZoomInOutlined
+                className="point-share-zoom"
+                style={{ fontSize: '40px', padding: '5.5px' }}
               />
-            </Row>
-            <Row className="teammate-two">
-              <img
-                className="teammate-one-avatar"
-                src={props.team.child2.AvatarURL}
-                alt="Child Avatar"
+            </div>
+            <div className="counter-box">
+              <p className="points-assigned">100</p>
+              <div className="increment-box">
+                <CaretUpOutlined
+                  className="increment-up"
+                  style={{ color: '#6CEAE6', fontSize: '30px' }}
+                />
+                <CaretDownOutlined
+                  className="increment-down"
+                  style={{ color: '#6CEAE6', fontSize: '30px' }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="point-share-drawing">
+            <p className="point-share-heading">DRAWING</p>
+            <div className="point-share-submission">
+              <img src={sampleDrawingSubmission} alt="child-one-drawing" />
+            </div>
+            <div className="point-share-zoom-container">
+              <ZoomInOutlined
+                className="point-share-zoom"
+                style={{ fontSize: '40px', padding: '5.5px' }}
               />
-            </Row>
-          </Col>
-          <Col>
-            <Row className="teammate-one-points">
-              <div className="submission-container">
-                <img
-                  className="submission"
-                  src={props.team.child1.ImgURL}
-                  alt="Submission"
-                  onClick={() =>
-                    openModal([{ ImgURL: props.team.child1.ImgURL }])
-                  }
+            </div>
+            <div className="counter-box">
+              <p className="points-assigned">100</p>
+              <div className="increment-box">
+                <CaretUpOutlined
+                  className="increment-up"
+                  style={{ color: '#6CEAE6', fontSize: '30px' }}
                 />
-                <InputNumber
-                  value={illustrationOnePoints}
-                  max={70}
-                  min={0}
-                  step={5}
-                  onChange={value =>
-                    sharePointHandler({
-                      value,
-                      pointSetter: setIllustrationOnePoints,
-                      a: storyTwoPoints,
-                      b: storyOnePoints,
-                      c: illustrationTwoPoints,
-                    })
-                  }
+                <CaretDownOutlined
+                  className="increment-down"
+                  style={{ color: '#6CEAE6', fontSize: '30px' }}
                 />
               </div>
-              <div className="submission-container">
-                <img
-                  className="submission"
-                  src={props.team.child1.Pages[0].PageURL}
-                  alt="Submission"
-                  onClick={() => openModal(props.team.child1.Pages)}
+            </div>
+          </div>
+        </div>
+
+        <div className="point-share-box child-two">
+          <div className="point-share-child-info">
+            <img src={childTwoImg} alt="child-two" />
+            <p className="point-share-heading">{`WHITE FOX`}</p>
+          </div>
+          <div className="point-share-story">
+            <p className="point-share-heading">STORY</p>
+            <div className="point-share-submission">
+              <img src={sampleWritingSubmission} alt="child-two-story" />
+            </div>
+            <div className="point-share-zoom-container">
+              <ZoomInOutlined
+                className="point-share-zoom"
+                style={{ fontSize: '40px', padding: '5.5px' }}
+              />
+            </div>
+            <div className="counter-box">
+              <p className="points-assigned">100</p>
+              <div className="increment-box">
+                <CaretUpOutlined
+                  className="increment-up"
+                  style={{ color: '#6CEAE6', fontSize: '30px' }}
                 />
-                <InputNumber
-                  value={storyOnePoints}
-                  max={70}
-                  min={0}
-                  step={5}
-                  onChange={value =>
-                    sharePointHandler({
-                      value,
-                      pointSetter: setStoryOnePoints,
-                      a: storyTwoPoints,
-                      b: illustrationOnePoints,
-                      c: illustrationTwoPoints,
-                    })
-                  }
-                />
-              </div>
-            </Row>
-            <Row className="teammate-two-points">
-              <div className="submission-container">
-                <img
-                  className="submission"
-                  src={props.team.child2.ImgURL}
-                  alt="Submission"
-                  onClick={() =>
-                    openModal([{ ImgURL: props.team.child2.ImgURL }])
-                  }
-                />
-                <InputNumber
-                  value={illustrationTwoPoints}
-                  max={70}
-                  min={0}
-                  step={5}
-                  onChange={value =>
-                    sharePointHandler({
-                      value,
-                      pointSetter: setIllustrationTwoPoints,
-                      a: storyTwoPoints,
-                      b: illustrationOnePoints,
-                      c: storyOnePoints,
-                    })
-                  }
+                <CaretDownOutlined
+                  className="increment-down"
+                  style={{ color: '#6CEAE6', fontSize: '30px' }}
                 />
               </div>
-              <div className="submission-container">
-                <img
-                  className="submission"
-                  src={props.team.child2.Pages[0].PageURL}
-                  alt="Submission"
-                  onClick={() => openModal(props.team.child2.Pages)}
-                />
-                <InputNumber
-                  value={storyTwoPoints}
-                  max={70}
-                  min={0}
-                  step={5}
-                  onChange={value =>
-                    sharePointHandler({
-                      value,
-                      pointSetter: setStoryTwoPoints,
-                      a: storyOnePoints,
-                      b: illustrationOnePoints,
-                      c: illustrationTwoPoints,
-                    })
-                  }
+            </div>
+          </div>
+          <div className="point-share-drawing">
+            <p className="point-share-heading">DRAWING</p>
+            <div className="point-share-submission">
+              <img
+                className="submission-image"
+                src={sampleDrawingSubmission}
+                alt="child-two-drawing"
+              />
+              <div className="point-share-zoom-container">
+                <ZoomInOutlined
+                  className="point-share-zoom"
+                  style={{ fontSize: '40px', padding: '5.5px' }}
                 />
               </div>
-            </Row>
-          </Col>
-        </Row>
-        <Button
-          selection="#eb7d5bbb"
-          className="match-up"
-          type="primary"
-          size="large"
-          onClick={formSubmit}
-        >
-          Match Up!
-        </Button>
-        <Button className="back-to-join-the-squad" onClick={backToJoin}>
-          Back
-        </Button>
+              <div className="counter-box">
+                <p className="points-assigned">100</p>
+                <div className="increment-box">
+                  <CaretUpOutlined
+                    className="increment-up"
+                    style={{ color: '#6CEAE6', fontSize: '30px' }}
+                  />
+                  <CaretDownOutlined
+                    className="increment-down"
+                    style={{ color: '#6CEAE6', fontSize: '30px' }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Button className="point-share-submit-button">Submit Points</Button>
       </div>
     </>
   );
