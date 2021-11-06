424946f3ba894ef17511a15c50d05a91277345d8
Stephen Aranda <75341073+OniWarrior@users.noreply.github.com>
2021-11-05 22:14:08 -0500

    Feature/build choose avatar component (#291)
    * Re-added missing header comp #59
    
    * added container and header for choose avatar #8-10
    
    * added ChangeYourAvatar.less
    
    * imported ChangeYourAvatar.less #3
    
    * added bangers and cursive font family
    
    * aligned header to center #4
    
    * added container for avatar cards 11
    
    * built container for avatar card #12
    
    * added lighting kid avatar #13
    
    * added id to image #14
    
    * replaced img elem with div elem #13
    
    * added heigth,width, and border to avatar card
    
    * added image to card
    
    * added background-size
    
    * added no repeat on image
    
    * added min height to image
    
    * centered image
    
    * changed height of the avatar card
    
    * changed width of the avatar card
    
    * added box sizing
    
    * Added box shadow to card
    
    * added another avatar card #15
    
    * added ruleset for captain falcon card
    
    * Added the image for captain falcon avatar
    
    * Added ruleset for the container of avatar cards
    
    * added another avatar card #18
    
    * added ruleset for shazaam kid avatar
    
    * Added another avatar card #21
    
    * added ruleset for blue whirlwind avatar
    
    * added another avatar card #24
    
    * added ruleset for pan on head avatar
    
    * Added another avatar card #27
    
    * added ruleset for purple hero kid
    
    * added another avatar card #30
    
    * added ruleset for frost kid
    
    * added another avatar card #33
    
    * added ruleset for flash kid
    
    * added flex wrap to card container #11
    
    * added another avatar card #36
    
    * added ruleset for fly girl
    
    * added another avatar card #39
    
    * added rule set for super girl
    
    * added flex flow wrap for cards
    
    * added spacing underneath cards #21
    
    * widened the cards #17
    
    * increased padding of cards
    
    * increased height of cards
    
    * preparing to add ruleset for avatar card hover
    
    * Scale up when hovering over card
    
    * preparing to add animation keyframe
    
    * Added starting point for animation #31
    
    * added 45 deg rotation to left #34
    
    * added a rotate back to center #38
    
    * added rotation right 45 deg #41
    
    * finished animation to rotate back to center #44
    
    * added the animation to ava card hover #26
    
    * added comment for the header styling
    
    * added comment for the container of avatar cards
    
    * added comment for the styling of avatar cards
    
    * added comment for avatar card hover
    
    * added comment for the animation keyframes
    
    * added comment for the image stylings
    
    * Added background color to avatar card
    
    * added a slight opacity to card

--- src/components/pages/ChangeAvatar/RenderChangeAvatar.js
+++ src/components/pages/ChangeAvatar/RenderChangeAvatar.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import { Header } from '../../common';
+import '../../../styles/less/ChangeYourAvatar.less';
 
 const RenderChangeAvatar = () => {
   return (
     <>
       <Header displayMenu={true} />
+      <div className="avatar-header">
+        <h2>Choose your avatar</h2>
+        <div className="avatar-cards-container">
+          <div className="avatar-card">
+            <div className="avatar-lighting-kid"></div>
+          </div>
+          <div className="avatar-card">
+            <div className="avatar-captain-falcon-kid"></div>
+          </div>
+          <div className="avatar-card">
+            <div className="avatar-shazaam-kid"></div>
+          </div>
+          <div className="avatar-card">
+            <div className="avatar-blue-whirlwind-kid"></div>
+          </div>
+          <div className="avatar-card">
+            <div className="avatar-pan-on-head-kid"></div>
+          </div>
+          <div className="avatar-card">
+            <div className="avatar-purple-hero-kid"></div>
+          </div>
+          <div className="avatar-card">
+            <div className="avatar-frost-kid"></div>
+          </div>
+          <div className="avatar-card">
+            <div className="avatar-flash-kid"></div>
+          </div>
+          <div className="avatar-card">
+            <div className="avatar-fly-girl-kid"></div>
+          </div>
+          <div className="avatar-card">
+            <div className="avatar-super-girl-kid"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
