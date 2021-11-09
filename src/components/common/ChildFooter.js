ba252eb0024677b1a96b43ac66d280cc9f3eb56a
Barry Porch <69640436+bporch84@users.noreply.github.com>
2021-11-09 12:17:39 -0500

    Update ChildFooter.js

--- src/components/common/ChildFooter.js
+++ src/components/common/ChildFooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 import '../../styles/less/ChildFooter.less';
 
 export default function ParentFooter(props) {
-  // if no need to scroll go get to bottom of the page -> pass `no-scroll` by props
+  // if no need to scroll go to the bottom of the page -> pass `no-scroll` by props
   const { layoutContainerCheck } = props;
 
   const curYear = new Date().getFullYear();
