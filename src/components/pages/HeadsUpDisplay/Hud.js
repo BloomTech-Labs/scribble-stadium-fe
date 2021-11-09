0f5c4e038dc584f1efd327e6f158f4a893620732
Julianisrel <52305213+Julianisrel@users.noreply.github.com>
2021-11-08 18:46:56 -0800

    Feature-child-gameplay-styles (#294)

--- src/components/pages/HeadsUpDisplay/Hud.js
+++ src/components/pages/HeadsUpDisplay/Hud.js
@@ -41,7 +41,13 @@ function Hud(props) {
               ${
                 completedActivity.length - 1 >= activities.indexOf(a) &&
                 'completedActivity'
-              }`}
+              }
+              ${
+                currentActivity !== a &&
+                completedActivity.length - 1 < activities.indexOf(a) &&
+                'restActive'
+              }
+              `}
             >
               <span>{a}</span>
             </div>
@@ -54,7 +60,7 @@ function Hud(props) {
         expandIcon={({ isActive }) => (
           <DownCircleFilled
             rotate={isActive ? 180 : 0}
-            style={{ fontSize: '18px' }}
+            style={{ fontSize: '35px' }}
           />
         )}
         className="site-collapse-custom-collapse"
