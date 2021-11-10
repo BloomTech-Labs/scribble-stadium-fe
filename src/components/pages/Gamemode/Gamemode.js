97852e3ad0263b7761c89a34ebb122e897e916fe
Fry-Fr <ryjlwdg89@gmail.com>
2021-11-10 03:07:16 -0500

    adds a back-btn

--- src/components/pages/Gamemode/Gamemode.js
+++ src/components/pages/Gamemode/Gamemode.js
@@ -45,6 +45,10 @@ const Gamemode = ({ ...props }) => {
     props.child.gamemode = ggm;
   };
 
+  const dashboard = () => {
+    push('/dashboard');
+  };
+
   const startSinglePlayerMode = e => {
     e.preventDefault();
     push('/gameplay/mission/read');
@@ -56,7 +60,18 @@ const Gamemode = ({ ...props }) => {
         ? reini()
         : !props.child.gamemode.sp && (
             <div className="dash-container">
+<<<<<<< HEAD
               <Header displayMenu={true} />
+=======
+              <Header />
+              <Button
+                className="back-btn"
+                style={{ margin: '1rem' }}
+                onClick={dashboard}
+              >
+                Back to Dashboard
+              </Button>
+>>>>>>> 886e5bf (adds a back-btn)
               <div className="single-button-container">
                 <Button className="single-btn" onClick={startSinglePlayerMode}>
                   Single Player
