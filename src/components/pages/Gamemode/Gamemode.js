d041771d363c535f88c3a938f7f16c082565d975
Barry Porch <69640436+bporch84@users.noreply.github.com>
2021-11-10 18:51:27 -0500

    add menu to header (#322)
    great catch Barry!

--- src/components/pages/Gamemode/Gamemode.js
+++ src/components/pages/Gamemode/Gamemode.js
@@ -57,7 +57,7 @@ const Gamemode = ({ ...props }) => {
         : props.child.gamemode.mode === 'select' &&
           !props.child.gamemode.sp && (
             <div className="dash-container">
-              <Header />
+              <Header displayMenu={true} />
               <div className="single-button-container">
                 <Button className="single-btn" onClick={startSinglePlayerMode}>
                   Single Player
