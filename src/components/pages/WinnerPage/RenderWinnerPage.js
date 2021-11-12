cba33d538712082092562ea3cc4943dab62edc17
kato hawkins <kristhahawkins@icloud.com>
2021-11-12 01:46:14 -0500

    adjusted div tags in order to organize code a little better for less

--- src/components/pages/WinnerPage/RenderWinnerPage.js
+++ src/components/pages/WinnerPage/RenderWinnerPage.js
@@ -5,6 +5,8 @@ import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import explosion from '../../../assets/images/gamemodeimg/explosion.png';
 import boyImg from '../../../assets/images/gamemodeimg/LightingKid.png';
+import PlayAgainButtonAnimation from './PlayAgainButtonAnimation';
+
 const RenderWinnerPage = props => {
   const { push } = useHistory();
 
@@ -15,20 +17,15 @@ const RenderWinnerPage = props => {
   return (
     <>
       <Header displayMenu={true} />
-      {/* REMOVE ONCE DESIGN WORK BEGINS - START */}
+
       <div className={'winner-container'}>
         <div className="winner-box">
+          <h1>Winner!</h1>
           <img src={explosion} alt="boom" className="explosion-img" />
-          <div className="winner-text">
-            Winner!
-            <div className="winner-avatar">
-              <img src={boyImg} alt="boyImg" className="winner-boy-img" />
-            </div>
-            <div className="winner-name">THUNDERBOLT</div>
-          </div>
+          <img src={boyImg} alt="boyImg" className="winner-boy-img" />
         </div>
+        <p> THUNDERBOLT</p>
       </div>
-      {/* REMOVE ONCE DESIGN WORK BEGINS - END */}
     </>
   );
 };
