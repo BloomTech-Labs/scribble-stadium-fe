4bd019022c19ae53e992372385b1b1522180c7e8
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-12-13 21:21:43 -0500

    added AntD Modal and Button components for basic modal. Added instructions to Modal for user to know how to navigate through the read, draw and write steps. Imported useEffect for local Modal state to handle visibility of Modal pop-up.
    Co-authored-by: Polly Yospan <porgroovy@gmail.com>
    Co-authored-by: Felipe Slaughter-Quintero <fslauq@gmail.com>

--- src/components/pages/GamePlay/GameMissionProgress.js
+++ src/components/pages/GamePlay/GameMissionProgress.js
@@ -1,14 +1,14 @@
 //** Import Modules */
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-
+import { Modal, Button } from 'antd';
 //** Import Assets */
 import completeIcon from '../../../assets/images/gamemodeimg/completed.png';
 
 export default function GameMissionProgress(props) {
   // Get the history object
   const history = useHistory();
-
+  const [isModalVisible, setIsModalVisible] = useState(false);
   // Get the current state
   const { currentStep } = props;
 
@@ -31,37 +31,86 @@ export default function GameMissionProgress(props) {
     }
   };
 
-  return (
-    <div id="mission-progress">
-      <h3>Your Mission</h3>
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
 
-      <div className="steps">
-        <StepButton
-          stepNum="1"
-          stepName="read"
-          currentStep={currentStep}
-          handleStepClick={handleStepClick}
-          isComplete={props.submissionData.HasRead}
-          stepBGColor="#c6fd7e"
-        />
-        <StepButton
-          stepNum="2"
-          stepName="draw"
-          currentStep={currentStep}
-          handleStepClick={handleStepClick}
-          isComplete={props.submissionData.HasDrawn}
-          requiredStep={props.submissionData.HasRead}
-          stepBGColor="#ff845d"
-        />
-        <StepButton
-          stepNum="3"
-          stepName="write"
-          currentStep={currentStep}
-          handleStepClick={handleStepClick}
-          isComplete={props.submissionData.HasWritten}
-          requiredStep={props.submissionData.HasRead}
-          stepBGColor="#ffd854"
-        />
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div>
+      <div>
+        <Button
+          style={{ margin: '1rem' }}
+          className="back-btn"
+          onClick={showModal}
+        >
+          Game Tips
+        </Button>
+        <Modal
+          title="Basic Modal"
+          bodyStyle={{ padding: '1rem' }}
+          visible={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <p>
+            Here are a few tips to help you navigate your way through the game!
+          </p>
+          <p>
+            * Click on the "Read," "Draw" or "Write" buttons to navigate through
+            the mission.
+          </p>
+          <p>
+            * Remember, you have to complete each step in the mission in order
+            to advance to the next step, so if you haven't completed the "Read"
+            step, you won't be able to click on "Draw" or "Write" to advance.
+          </p>
+          <p>
+            * Once you have completed all the steps, you can click back and
+            forth between all of them to look over your work.
+          </p>
+          <p>* Click on the "Game Tips" button to see these tips again!</p>
+        </Modal>
+      </div>
+      {/* </div> */}
+      <div id="mission-progress">
+        <h3>Your Mission</h3>
+
+        <div className="steps">
+          <StepButton
+            stepNum="1"
+            stepName="read"
+            currentStep={currentStep}
+            handleStepClick={handleStepClick}
+            isComplete={props.submissionData.HasRead}
+            stepBGColor="#c6fd7e"
+          />
+          <StepButton
+            stepNum="2"
+            stepName="draw"
+            currentStep={currentStep}
+            handleStepClick={handleStepClick}
+            isComplete={props.submissionData.HasDrawn}
+            requiredStep={props.submissionData.HasRead}
+            stepBGColor="#ff845d"
+          />
+          <StepButton
+            stepNum="3"
+            stepName="write"
+            currentStep={currentStep}
+            handleStepClick={handleStepClick}
+            isComplete={props.submissionData.HasWritten}
+            requiredStep={props.submissionData.HasRead}
+            stepBGColor="#ffd854"
+          />
+        </div>
       </div>
     </div>
   );
