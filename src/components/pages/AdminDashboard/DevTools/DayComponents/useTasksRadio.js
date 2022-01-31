2aa7f930ffae37eed68fe5326e65d676fa404e06
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2022-01-31 16:50:34 -0500

    No error after deleting DevTools directory

--- src/components/pages/AdminDashboard/DevTools/DayComponents/useTasksRadio.js
+++ src/components/pages/AdminDashboard/DevTools/DayComponents/useTasksRadio.js
@@ -1,44 +1,44 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-const useTasksRadio = givenValue => {
-  const [value, setValue] = useState(givenValue);
+// const useTasksRadio = givenValue => {
+//   const [value, setValue] = useState(givenValue);
 
-  let radioTasks;
+//   let radioTasks;
 
-  switch (value) {
-    case 0:
-      radioTasks = {
-        hasRead: false,
-        hasDrawn: false,
-        hasWritten: false,
-      };
-      break;
-    case 1:
-      radioTasks = {
-        hasRead: true,
-        hasDrawn: false,
-        hasWritten: false,
-      };
-      break;
-    case 2:
-      radioTasks = {
-        hasRead: true,
-        hasDrawn: true,
-        hasWritten: false,
-      };
-      break;
-    case 3:
-      radioTasks = {
-        hasRead: true,
-        hasDrawn: true,
-        hasWritten: true,
-      };
-      break;
-    default:
-      return null;
-  }
+//   switch (value) {
+//     case 0:
+//       radioTasks = {
+//         hasRead: false,
+//         hasDrawn: false,
+//         hasWritten: false,
+//       };
+//       break;
+//     case 1:
+//       radioTasks = {
+//         hasRead: true,
+//         hasDrawn: false,
+//         hasWritten: false,
+//       };
+//       break;
+//     case 2:
+//       radioTasks = {
+//         hasRead: true,
+//         hasDrawn: true,
+//         hasWritten: false,
+//       };
+//       break;
+//     case 3:
+//       radioTasks = {
+//         hasRead: true,
+//         hasDrawn: true,
+//         hasWritten: true,
+//       };
+//       break;
+//     default:
+//       return null;
+//   }
 
-  return [radioTasks, value, setValue];
-};
+//   return [radioTasks, value, setValue];
+// };
 
-export default useTasksRadio;
+// export default useTasksRadio;
