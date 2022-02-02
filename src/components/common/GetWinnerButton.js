e38c8704d54fd50bd47fb9fb11361e639bc69d61
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2022-02-01 22:15:59 -0700

    completed draft of Get Winner button

--- src/components/common/GetWinnerButton.js
+++ src/components/common/GetWinnerButton.js
@@ -2,20 +2,21 @@ import React from 'react';
 
 // import PropTypes from 'prop-types'
 
-const getWinner = props => {
+const GetWinnerButton = props => {
   const handleSubmit = e => {
     e.preventDefault();
     axios
-      .get()
-      .then(res => {})
+      .get('http://localhost:5000/api/game/results?squadId=1')
+      .then(res => {
+        console.log(res.data);
+      })
       .catch(err => console.log(err));
   };
-  console.log(handleSubmit);
 
   return <button onClick={handleSubmit}>Get Winner!</button>;
 };
 
-export default getWinner;
+export default GetWinnerButton;
 
 // getWinner.PropTypes = {
 //     handleClick: PropTypes.func,
