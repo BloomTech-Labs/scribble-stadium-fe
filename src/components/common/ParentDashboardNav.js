77efc2e59f25211573ddb60d0c086ed9d0554d6b
Rocky Fierro <finnslighthouse@gmail.com>
2022-02-17 20:57:03 -0800

    added browserrouter and routes to parent dashboard nav.

--- src/components/common/ParentDashboardNav.js
+++ src/components/common/ParentDashboardNav.js
@@ -2,29 +2,40 @@ import React, { useEffect, useState } from 'react';
 import { Layout } from 'antd';
 import { useAuth0 } from '@auth0/auth0-react';
 import { getProfileData } from '../../../api';
-import ParentNavTopBar from '../../common/ParentNavTopBar';
 import NewProgressCharts from '../../common/NewProgressCharts';
 import NewChildCard from '../../common/NewChildCard';
 import AccountSettings from '../AccountSettings/AccountSettingsContainer';
-import { connect } from 'react-redux';
-import { setParent } from '../../../state/actions/parentActions';
-import RenderWordCloud from '../WordCloud';
 import ParentFooter from '../../common/ParentFooter';
 import ChooseChildModal from './ChooseChildModal';
+import { connect } from 'react-redux';
+import { setParent } from '../../../state/actions/parentActions';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // TEMPORARY HARD CODE - import of image assets - remove when pulling state from BE or globally
 import pinkyWinky from '../../../assets/images/hero_images/hero10.png';
 import submarineBoy from '../../../assets/images/hero_images/hero3.png';
 import dad from '../../../assets/images/hero_images/hero5.png';
-import ParentNavSider from '../../common/ParentNavSider';
 
 const ParentDashboardNav = props => {
   return (
-    <div>
-      <nav>
-        <ul></ul>
-      </nav>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <ChooseChildModal />
+        </Route>
+        <Route path="/">
+          <ParentFooter />
+        </Route>
+        <Route path="/">
+          <NewProgressCharts />
+        </Route>
+        <Route path="/">
+          <NewChildCard />
+        </Route>
+        <Route path="/">
+          <AccountSettings />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default ParentDashboardNav;
