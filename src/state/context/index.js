24459542fb894514874fd27e74b246d1c0598d22
Rika Ciminieri <rikaciminieri@gmail.com>
2022-02-24 20:20:05 -0800

    Child context created

--- src/state/context/index.js
+++ src/state/context/index.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useState } from 'react';
 
 // Parent Context
 // ID: string, NAME: string, EMAIL: string, PIN: string, CHILDREN: Array<String>
@@ -31,3 +31,72 @@ export const ParentContextProvider = ({ comps }) => {
     </ParentContext.Provider>
   );
 };
+
+// Child Context
+export const ChildContext = createContext();
+
+export const ChildContextProvider = ({ comps }) => {
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [pin, setPin] = useState('');
+  const [isDyslexic, setIsDyslexic] = useState('');
+  const [parentId, setParentId] = useState('');
+  const [cohortId, setCohortId] = useState('');
+  const [avatarId, setAvatarId] = useState('');
+  const [totalPoints, setTotalPoints] = useState('');
+  const [wins, setWins] = useState('');
+  const [losses, setLosses] = useState('');
+  const [ballots, setBallots] = useState('');
+  const [votesRemaining, setVotesRemaining] = useState('');
+  const [achievements, setAcievements] = useState('');
+  const [gradeLevel, setGradeLevel] = useState('');
+  const [avatarURL, setAvatarURL] = useState('');
+  const [streaks, setStreaks] = useState('');
+  const [notifications, setNotifications] = useState('');
+  const [events, setEvents] = useState('');
+};
+
+return (
+  <ChildContext.Provider
+    value={{
+      id,
+      setId,
+      name,
+      setName,
+      pin,
+      setPin,
+      isDyslexic,
+      setIsDyslexic,
+      parentId,
+      setParentId,
+      cohortId,
+      setCohortId,
+      avatarId,
+      setAvatarId,
+      totalPoints,
+      setTotalPoints,
+      wins,
+      setWins,
+      losses,
+      setLosses,
+      ballots,
+      setBallots,
+      votesRemaining,
+      setVotesRemaining,
+      achievements,
+      setAcievements,
+      gradeLevel,
+      setGradeLevel,
+      avatarURL,
+      setAvatarURL,
+      streaks,
+      setStreaks,
+      notifications,
+      setNotifications,
+      events,
+      setEvents,
+    }}
+  >
+    {comps}
+  </ChildContext.Provider>
+);
