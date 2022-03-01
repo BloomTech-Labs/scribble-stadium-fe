dc2fa756f582dbe9b7054e3d56bdc68058bf7981
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2022-03-01 10:32:04 -0500

    updated AdminHeader.js

--- src/components/common/Admin/AdminHeader.js
+++ src/components/common/Admin/AdminHeader.js
@@ -1,26 +1,35 @@
-import React from "react"
-import { useAuth0 } from "@auth0/auth0-react";
-import "./styles/Header.css";
-import {useNavigate} from "react-router-dom";
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'antd';
 
 const Header = () => {
-    const { user, isAuthenticated, logout } = useAuth0();
-    const navigate = useNavigate();
-  
-    return (
-        isAuthenticated && ( // checks for authenticated user
-            <div className="header">
-                <h4>Admin: {user.name}</h4>
-                <div>
-                    Story Squad Logo
-                </div>
-                <div className="buttons">
-                    <button onClick={() => {navigate("/admindashboard")}}>Home</button>
-                    <button onClick={()=> logout()}>Log Out</button>
-                </div>      
-            </div> 
-        )
-    )
-}
+  const { user, isAuthenticated, logout } = useAuth0();
+  const navigate = useNavigate();
 
-export default Header
+  return (
+    isAuthenticated && (
+      <div className="header">
+        <h4>Admin: {user.name}</h4>
+        <div>
+          <img
+            src="https://raw.githubusercontent.com/BloomTech-Labs/scribble-stadium-fe/main/public/assets/Login-Explosion-Final.png"
+            alt="Story Squad Logo"
+          />
+        </div>
+        <div className="buttons">
+          <Button
+            onClick={() => {
+              navigate('/admin');
+            }}
+          >
+            Home
+          </Button>
+          <Button onClick={() => logout()}>Log Out</Button>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default Header;
