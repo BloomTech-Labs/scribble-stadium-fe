3e2dd7b8dc0a026b9f93377d177be240d4cb157d
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2022-03-01 11:06:27 -0500

    updated AdminSidebar.js

--- src/components/common/Admin/AdminSidebar.js
+++ src/components/common/Admin/AdminSidebar.js
@@ -1,29 +1,34 @@
-import React,{useState} from 'react';
+import { Button } from 'antd';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SideBar() {
-    const [isActive, setActive] = useState("true");
+  const [isActive, setActive] = useState('true');
 
-    const handleToggle = () => {
-        setActive(!isActive);
-      };
-    return (
-        <div>
-            <nav className={isActive? "wide-sidebar": "narrow-sidebar"}>
-                <div className='navlinks'>
-                    <div className='navlink'>
-                        <Link to="/admindashboard"> <img src="" alt='dashboard-icon'/>{isActive? "Dashboard":null} </Link>
-                    </div>
-                    <div className='navlink'>
-                        <Link to="/storymanager"> <img src="" alt='story-manager-icon'/>{isActive? "Story Manager":null} </Link>
-                    </div>
-                </div>
-                <div 
-                    className='hamburger-menu'
-                    onClick={handleToggle}>
-                    <img src="" alt='hamburger menu'/>
-                </div>
-            </nav>
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+  return (
+    <div>
+      <nav className={isActive ? 'wide-sidebar' : 'narrow-sidebar'}>
+        <div className="navlinks">
+          <div>
+            <Link to="/admindashboard">
+              {' '}
+              <Button>D</Button>
+              {isActive ? 'Dashboard' : null}{' '}
+            </Link>
+          </div>
+          <div>
+            <Link to="/storymanager">
+              {' '}
+              <Button>SM</Button>
+              {isActive ? 'Story Manager' : null}{' '}
+            </Link>
+          </div>
         </div>
-    );
+        <Button onClick={handleToggle}>{isActive ? 'Close' : 'Open'}</Button>
+      </nav>
+    </div>
+  );
 }
