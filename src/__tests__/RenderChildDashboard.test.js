327d0c761d7b964969751c54b0932fb6e1f8fcee
Greg Brewton <greg@studio42dev.com>
2022-04-06 21:27:09 -0500

    Refactors render import

--- src/__tests__/RenderChildDashboard.test.js
+++ src/__tests__/RenderChildDashboard.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import RenderChildDashboard from '../components/pages/ChildDashboard/RenderChildDashboard.js';
-import { screen } from '@testing-library/react';
-import { render as renderWithRedux } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import persistReducer from '../state/reducers/index';
@@ -9,7 +8,7 @@ import persistReducer from '../state/reducers/index';
 describe('RenderChildDashboard component', () => {
   it('should render without errors', () => {
     const store = createStore(persistReducer);
-    renderWithRedux(
+    render(
       <Provider store={store}>
         <RenderChildDashboard />
       </Provider>
