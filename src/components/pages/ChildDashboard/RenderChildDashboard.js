858947f78be329f4bfd30f9c81c07d69e4bc6366
Greg Brewton <greg@studio42dev.com>
2022-04-12 19:28:39 -0600

    fixes import of HUD component

--- src/components/pages/ChildDashboard/RenderChildDashboard.js
+++ src/components/pages/ChildDashboard/RenderChildDashboard.js
@@ -5,7 +5,7 @@ import { Row, Col } from 'antd';
 import { useHistory } from 'react-router-dom';
 import { InstructionsModal } from '../../common';
 import { modalInstructions } from '../../../utils/helpers';
-import Hud from '../GamePlay/index';
+import { HUD } from '../HeadsUpDisplay/index';
 import change_your_avatar from '../../../assets/images/child_dashboard_images/change_your_avatar.svg';
 import leaderboard_icon from '../../../assets/images/child_dashboard_images/leaderboard_icon.png';
 import data from '../../../data.json';
@@ -62,7 +62,7 @@ const RenderChildDashboard = props => {
         title={'Scribble Stadium'}
         data-testid="child-dashboard"
       />
-      <Hud
+      <HUD
         completedActivity={['Read', 'Draw', 'Write']}
         currentActivity={'Squad Up'}
         currentBar={'bar2'}
