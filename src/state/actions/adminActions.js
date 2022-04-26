873b5ac247229bb2b0ab24173f27da4d04d541bd
Hamida Sh <sharifovahamida@gmail.com>
2022-04-26 00:00:30 -0700

    Separated BacklogStory css, added all new css files into index file, added new actions to handle status update

--- src/state/actions/adminActions.js
+++ src/state/actions/adminActions.js
@@ -1,5 +1,10 @@
 export const FETCH_STORIES = 'FETCH_STORIES';
+export const UPDATE_STORY_STATUS = 'UPDATE_STORY_STATUS';
 
 export const fetchStories = () => dispatch => {
   dispatch({ type: FETCH_STORIES });
+};
+
+export const updateStories = updatedStories => dispatch => {
+  dispatch({ type: UPDATE_STORY_STATUS, payload: updatedStories });
 };
