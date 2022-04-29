389ee4d9caeb37811f1e5390b5ae4e42d5586c4d
Christopher Amos <christopheramos@ChristohersMini.attlocal.net>
2022-04-28 21:29:30 -0400

    fixed a component piece

--- src/__tests__/CropCloud.test.js
+++ src/__tests__/CropCloud.test.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { screen, render } from '@testing-library/react';
-import cropcloud from 'src/components/pages/WordCloud/CropCloud.js';
+import cropcloud from '../components/pages/WordCloud/CropCloud.js';
 import { Provider } from 'react-redux';
 
 describe('Croupcloud component', () => {});
