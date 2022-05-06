efd8178b9aee2d5f8cd4bb24ccccc0bac0c52d26
Rocky Fierro <finnslighthouse@gmail.com>
2022-05-06 16:56:53 -0700

    no-unused-vars
    unused button,
    unused function

--- src/components/pages/WinnerPage/RenderWinnerPage.js
+++ src/components/pages/WinnerPage/RenderWinnerPage.js
@@ -1,7 +1,11 @@
 // following code is commented out to prevent warnings during compilation
 import React from 'react';
 import { Header } from '../../common';
+<<<<<<< refs/remotes/origin/main
 // import { useHistory } from 'react-router-dom';
+=======
+import { useHistory } from 'react-router-dom';
+>>>>>>> no-unused-vars
 import { connect } from 'react-redux';
 import explosion from '../../../assets/images/gamemodeimg/explosion.png';
 import boyImg from '../../../assets/images/hero_images/hero1.png';
@@ -10,9 +14,15 @@ import PlayAgainButtonAnimation from './PlayAgainButtonAnimation';
 const RenderWinnerPage = props => {
   // const { push } = useHistory();
 
+<<<<<<< refs/remotes/origin/main
   // const goToChildDashboard = () => {
   //   push('/child/dashboard');
   // };
+=======
+//  const goToChildDashboard = () => {
+//    push('/child/dashboard');
+//  };
+>>>>>>> no-unused-vars
 
   return (
     <>
