a9be283b5a5eba3e876736928dd2c7694cb2158a
ashtilawat23 <ashtilawat23@gmail.com>
2022-05-06 12:24:28 -0400

    onboarding messaged fixed for windows

--- start.js
+++ start.js
@@ -1,19 +1,24 @@
-const { spawn } = require("child_process");
-const { Buffer } = require("buffer");
- 
-const ls = spawn("npx",["craco","start"]);
+/* eslint-disable */
 
-ls.stdout.on("data", data => {
-    console.log(`${data}`);
-    if (data.toString().includes("To ignore, add")) {
-        console.log(Buffer.from('VGltZTJjb2RlIQ==', 'base64').toString('binary'));
-    }
+const { spawn } = require('child_process');
+const { Buffer } = require('buffer');
+
+const ls = spawn(/^win/.test(process.platform) ? 'npx.cmd' : 'npx', [
+  'craco',
+  'start',
+]);
+
+ls.stdout.on('data', data => {
+  console.log(`${data}`);
+  if (data.toString().includes('To ignore, add')) {
+    console.log(Buffer.from('VGltZTJjb2RlIQ==', 'base64').toString('binary'));
+  }
 });
 
-ls.stderr.on("ERROR", data => {
-    console.log(`stderr: ${data}`);
+ls.stderr.on('ERROR', data => {
+  console.log(`stderr: ${data}`);
 });
 
-ls.on('error', (error) => {
-    console.log(`error: ${error.message}`);
+ls.on('error', error => {
+  console.log(`error: ${error.message}`);
 });
