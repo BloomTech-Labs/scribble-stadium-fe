c06571b26d4ab1de774ce706425ec997e4636fb3
Rocky Fierro <finnslighthouse@gmail.com>
2022-05-06 12:43:59 -0700

    commented out unused funct
    added (blank) default case to switch statement

--- src/components/pages/WinnerPage/PlayAgainButtonAnimation.js
+++ src/components/pages/WinnerPage/PlayAgainButtonAnimation.js
@@ -4,9 +4,15 @@ import { useHistory } from 'react-router-dom';
 const PlayAgainButtonAnimation = () => {
   const { push } = useHistory();
 
+<<<<<<< refs/remotes/origin/main
   // const goToChildDashboard = () => {
   //   push('/child/dashboard');
   // };
+=======
+//  const goToChildDashboard = () => {
+//   push('/child/dashboard');
+//  };
+>>>>>>> commented out unused funct
   const playAgain = () => {
     push('/child/play-again');
   };
@@ -16,8 +22,12 @@ const PlayAgainButtonAnimation = () => {
       case 'animationiteration':
         l.textContent = `New loop started at time ${event.elapsedTime}`;
         break;
+<<<<<<< refs/remotes/origin/main
       default:
         l.textContent = `Animation event of type ${event.type}`; // no default case was included, so I made my best guess while squashing bugs.
+=======
+	default: ;
+>>>>>>> commented out unused funct
     }
     document.getElementById('output').appendChild(l);
   }
