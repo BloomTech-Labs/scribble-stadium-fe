0744466c53052e56302130af87049ed9a009d222
Stan <stan2dor@gmail.com>
2022-05-27 10:58:56 -0700

    replaced header components

--- src/components/common/Admin/AdminHeader.js
+++ src/components/common/Admin/AdminHeader.js
@@ -1,50 +1,80 @@
 import React from 'react';
+import { Typography, Menu, Dropdown } from 'antd';
+import { Link, useHistory } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import { connect } from 'react-redux';
+import { global } from '../../../state/actions';
 
-// import '../../../styles/less/Admin.less';
+import parent_avatar from '../../../assets/icons/parent_avatar.svg';
 
-import { Badge, Avatar } from 'antd';
-import {
-  HomeOutlined,
-  UserOutlined,
-  QuestionCircleOutlined,
-  BellOutlined,
-} from '@ant-design/icons';
+const { Title } = Typography;
 
-import { Link } from 'react-router-dom';
+const ParentMenu = props => {
+  const { push } = useHistory();
+  const { logout } = useAuth0();
+  const { clearUsers, ...rest } = props;
 
-const AdminHeader = () => {
-  const { user } = useAuth0();
-  // const history = useHistory();
+  const switchUsers = () => {
+    clearUsers();
+    push('/');
+  };
 
   return (
-    <div className="header">
-      <div>
-        <Link to="/">
-          <h1 className="header-logo">{'STORY SQUAD'}</h1>
-        </Link>
-      </div>
-
-      <div className="header-icons">
-        <Badge count={11}>
-          <Avatar
-            style={{ backgroundColor: '#87d068' }}
-            icon={<UserOutlined />}
-          />
-        </Badge>
-
-        <h4>Welcome back, {user.name}</h4>
-
-        <div className="header-nav">
-          <HomeOutlined style={{ fontSize: 18 }} />
-          <QuestionCircleOutlined style={{ fontSize: 18 }} />
-          <Badge dot>
-            <BellOutlined style={{ fontSize: 18 }} />
-          </Badge>
-        </div>
-      </div>
-    </div>
+    <Menu {...rest}>
+      <Menu.Item key="1" onClick={switchUsers}>
+        Change User
+      </Menu.Item>
+      <Menu.Item
+        key="2"
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
+        Logout
+      </Menu.Item>
+    </Menu>
   );
 };
 
-export default AdminHeader;
+const AdminHeader = props => {
+  return (
+    <nav className="admin-nav-top-bar">
+      <Link to="/">
+        <Title className="title navbar-logo" level={1}>
+          Story Squad
+        </Title>
+      </Link>
+      <div className="nav-right">
+        <div className="link-container"></div>
+        <div className="straight-bar"></div>
+        <span className="welcome-back-msg">
+          {props.parent &&
+            `Welcome back, ` +
+              (props.parent.name == null ? 'Admin' : `${props.parent.name}`) +
+              `!`}
+        </span>
+        <Dropdown
+          overlay={<ParentMenu clearUsers={props.clearUsers} />}
+          trigger={['hover']}
+          placement="bottom"
+        >
+          <a
+            href="/"
+            className="parent-avatar"
+            data-testid="parent-avatar"
+            onClick={e => e.preventDefault()}
+          >
+            <img src={parent_avatar} alt="Dropdown Menu" />
+          </a>
+        </Dropdown>
+      </div>
+    </nav>
+  );
+};
+
+export default connect(
+  state => ({
+    parent: state.parent,
+  }),
+  {
+    clearUsers: global.clearUsers,
+  }
+)(AdminHeader);
