9c00276ad3848405b62347ce66f257a30eba3d1f
Stan <stan2dor@gmail.com>
2022-06-24 13:53:48 -0700

    Moderator Dashboard innitial commit

---
+++ src/components/pages/Moderator/Moderator.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import AdminHeader from '../../common/Admin/AdminHeader';
+import ModeratorSideBar from '../Moderator/ModeratorSidebar';
+import TransferDrawing from './TransferDrawing';
+import ParentFooter from '../../common/ParentFooter';
+// import StoryManager from '../Admin/StoryManager';
+
+const Moderator = () => {
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    isAuthenticated && (
+      <Router>
+        <AdminHeader />
+        <div className="main">
+          <ModeratorSideBar />
+          <div className="container">
+            <h1>I am a Moderator</h1>
+            <TransferDrawing />
+            {/* <StoryManager /> */}
+          </div>
+        </div>
+        <div>
+          <ParentFooter layoutContainerCheck={'scroll'} />
+        </div>
+      </Router>
+    )
+  );
+};
+
+export default Moderator;
