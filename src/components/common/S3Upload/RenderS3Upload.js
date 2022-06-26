3658718cb2fbf8217a37249105b4fdd32365a5bf
Joe Hernandez <j.t.hernandez999@gmail.com>
2022-06-26 12:25:10 -0700

    removed unused auth import

--- src/components/common/S3Upload/RenderS3Upload.js
+++ src/components/common/S3Upload/RenderS3Upload.js
@@ -1,10 +1,9 @@
 import React from 'react';
-// import { useAuth0 } from '@auth0/auth0-react';
+
 import { postNewUpload } from '../../../api';
 
 export default function S3UploadButton() {
   const [file, setFile] = React.useState('');
-  // const { user } = useAuth0();
 
   const fileSelected = e => {
     const selectedFile = e.target.files[0];
