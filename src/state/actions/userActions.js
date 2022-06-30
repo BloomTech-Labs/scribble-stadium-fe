b66ac43e44f2fe253fd726c04c0541ea3ab6fdfa
ashtilawat23 <ashtilawat23@gmail.com>
2022-06-30 01:32:20 -0400

    fix on userReducer

--- src/state/actions/userActions.js
+++ src/state/actions/userActions.js
@@ -2,27 +2,11 @@ import axiosWithAuth from '../../utils/axiosWithAuth';
 export const GET_PROFILE = 'GET_PROFILE';
 export const SET_ERROR = 'SET_ERROR';
 
-function parseJwt(token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(
-    window
-      .atob(base64)
-      .split('')
-      .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join('')
-  );
-
-  return JSON.parse(jsonPayload);
-}
-
-export const getProfile = () => async dispatch => {
+export const getProfile = sub => async dispatch => {
   if (localStorage.getItem('isAuthenticated')) {
     console.log('hit');
     axiosWithAuth()
-      .get(`/profiles/${parseJwt(localStorage.getItem('idToken')).sub}`)
+      .get(`/profiles/${sub}`)
       .then(res => {
         dispatch({
           type: GET_PROFILE,
