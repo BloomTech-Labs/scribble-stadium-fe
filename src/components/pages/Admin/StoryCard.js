2cb268c8d60956b03cf38764b90079b06ebad9da
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-09-09 18:11:03 -0400

    BL-874 added edit icon

--- src/components/pages/Admin/StoryCard.js
+++ src/components/pages/Admin/StoryCard.js
@@ -6,11 +6,18 @@ const StoryCard = (props) => {
 
     return (
         <div>
-           <Card title={storyData.title}>
+            <Card 
+                title={storyData.title}
+                extra={<Button>Edit</Button>}
+                style={{
+                    width: 500,
+                    marginBottom: 10,
+                }}
+            >
                 <p>Author: {storyData.author}</p>
                 <p>Description: {storyData.description}</p>
                 <p>Date Posted: {storyData.date_posted}</p>
-           </Card>
+            </Card>
         </div>
     )
 }
